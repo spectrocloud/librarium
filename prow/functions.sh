e0876f80d1b16eb0e1ff46da993776b6c4d7694c
Roshani Rathi <rrroshani227@gmail.com>
2020-05-30 20:56:17 +0530

    Adding commenter code

--- prow/functions.sh
+++ prow/functions.sh
@@ -4,6 +4,17 @@
 # Variables expected from CI - PULL_NUMBER , JOB_TYPE , ARTIFACTS , SONAR_SCAN_TOKEN, SONARQUBE_URL, DOCKER_REGISTRY
 set -x
 
+commenter() {
+	export GITHUB_TOKEN=$ACCESS_TOKEN_PWD
+	export GITHUB_OWNER=$REPO_OWNER
+	export GITHUB_REPO=$REPO_NAME
+	export GITHUB_COMMENT_TYPE=pr
+	export GITHUB_PR_ISSUE_NUMBER=$PULL_NUMBER
+	export GITHUB_COMMENT_FORMAT="Build logs for Job ${JOB_NAME} can be found here: {{.}}"
+	export GITHUB_COMMENT="http://mayflower.spectrocloud.com/log?job=${JOB_NAME}&id=${BUILD_NUMBER}"
+	github-commenter
+}
+
 build_docs() {
 	make initialize
 	make start
