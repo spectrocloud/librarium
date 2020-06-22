9b18deb009ea1b49c2d6a6f8e5ee458782fcc196
roshanirathi <42164609+roshanirathi@users.noreply.github.com>
2020-06-22 19:04:56 +0530

    Post-submit job (#67)

---
+++ prow/postsubmit.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+########################################
+# Postsubmit script triggered by Prow. #
+########################################
+
+set -x
+
+source prow/functions.sh
+
+post_submit
+
