2e7d9e29d3095288cc91eff567bef78aa8ca76ca
Mihail Iftodi <mihail@spectrocloud.com>
2020-10-22 14:31:53 +0300

    deprecated tag for fields

--- packages/api/src/components/CodeHighlight.js
+++ packages/api/src/components/CodeHighlight.js
@@ -1,21 +1,42 @@
 import React from "react";
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import prismTheme from 'prism-react-renderer/themes/nightOwlLight';
+import styled from "styled-components";
+
+const DeprecatedTag = styled.span`
+  background: #cfcfcf;
+  padding: 3px 5px;
+  border-radius: 4px;
+  color: #fff;
+  font-size: 12px;
+  margin-left: 5px;
+`;
 
 function CodeHighlight({ code, language = "json", theme=prismTheme }) {
   return (
     <Highlight {...defaultProps} code={code} language={language} theme={theme}>
-      {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={style}>
-          {tokens.map((line, i) => (
+      {({ className, style, tokens, getLineProps, getTokenProps }) =>
+        {
+        return (<pre className={className} style={style}>
+          {tokens.map((line, i) => {
+            const deprecatedLineIndex = line.findIndex(token => token.content.includes("deprecated"));
+            if(deprecatedLineIndex >= 0) {
+              const content = line[deprecatedLineIndex].content;
+              line[deprecatedLineIndex].content = content.substring(0, content.length - 12) + '"';
+              line.splice(deprecatedLineIndex + 2, 0, {content: "deprecated", types: ["string"]});
+            }
+            return (
             <div {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
+                  token.content === "deprecated" ?
+                  <DeprecatedTag>deprecated</DeprecatedTag> :
+                  <span {...getTokenProps({ token, key })} />
               ))}
             </div>
-          ))}
-        </pre>
-      )}
+          )})}
+        </pre>)
+          }
+      }
     </Highlight>
   )
 }
