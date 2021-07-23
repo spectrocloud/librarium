072d59d4768bab5a2959917506b1e9ac80ae46a4
Roshani Rathi <rrroshani227@gmail.com>
2021-07-23 13:04:26 +0530

    add auto-reconf

--- prow/functions.sh
+++ prow/functions.sh
@@ -6,6 +6,7 @@ set -x
 
 # Initialize & Build  docs
 build_docs() {
+	apk add --no-cache autoconf
 	make initialize
 	GATSBY_ALGOLIA_APP_ID=${GATSBY_ALGOLIA_APP_ID} GATSBY_ALGOLIA_SEARCH_KEY=${GATSBY_ALGOLIA_SEARCH_KEY} ALGOLIA_ADMIN_KEY=${ALGOLIA_ADMIN_KEY} make build
 }
