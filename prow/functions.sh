19439cc023417d732467f2017d8b55a6e43c4e4e
Roshani Rathi <rrroshani227@gmail.com>
2021-07-23 13:36:02 +0530

    install autoconf with automake

--- prow/functions.sh
+++ prow/functions.sh
@@ -6,7 +6,7 @@ set -x
 
 # Initialize & Build  docs
 build_docs() {
-	apk add --no-cache automake
+	apk add --no-cache automake autoconf
 	make initialize
 	GATSBY_ALGOLIA_APP_ID=${GATSBY_ALGOLIA_APP_ID} GATSBY_ALGOLIA_SEARCH_KEY=${GATSBY_ALGOLIA_SEARCH_KEY} ALGOLIA_ADMIN_KEY=${ALGOLIA_ADMIN_KEY} make build
 }
