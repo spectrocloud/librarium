a7f923cf229c731ea863d375b8881113802d15bb
Karl Cardenas <karl@spectrocloud.com>
2023-03-27 17:34:04 -0700

    ci: docs url checker

--- scripts/pr-comments.sh
+++ scripts/pr-comments.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 
-# Obtain pull request number from environment variable
-PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
-echo "Pull request number: $PR_NUMBER"
+# Obtain pull request number from environment variable if available
+if [ -n "$GITHUB_EVENT_PATH" ]; then
+  PR_NUMBER=$(jq --raw-output .pull_request.number "$GITHUB_EVENT_PATH")
+fi
 
-# Set variables for GitHub API
-ACCESS_TOKEN=$ACCESS_TOKEN
-OWNER=spectrocloud
-REPO=librarium
+# If pull request number is not available, retrieve it from the GitHub API
+if [ -z "$PR_NUMBER" ]; then
+  # Get the branch name from the GITHUB_REF environment variable
+  branch_name=$(echo $GITHUB_REF | awk -F'/' '{print $3}')
 
-# Read JSON file contents into a variable
-JSON_CONTENT=$(cat link_report.json)
+  # Use the GitHub API to retrieve information about the pull request
+  pull_request=$(curl -s -H "Authorization: Bearer $ACCESS_TOKEN" "https://api.github.com/repos/$OWNER/$REPO/pulls?head=$GITHUB_REPOSITORY:$branch_name")
 
-# Format comment with JSON content
-COMMENT="```
+  # Extract the pull request number from the API response
+  PR_NUMBER=$(echo $pull_request | jq -r '.[0].number')
+fi
+
+# Print the pull request number if it is not null
+if [ ! -z "$PR_NUMBER" ]; then
+  echo "Posting comment to pull request #$PR_NUMBER"
+
+  # Read JSON file contents into a variable
+  JSON_CONTENT=$(cat link_report.json)
+
+  # Format comment with JSON content
+  COMMENT="```
 $JSON_CONTENT
 ```"
 
-echo "Posting comment to pull request #$PR_NUMBER"
-
-# Post comment to pull request using GitHub API
-curl -X POST \
--H "Authorization: token $ACCESS_TOKEN" \
--d "{\"body\":\"$COMMENT\"}" \
-"https://api.github.com/repos/$OWNER/$REPO/issues/$PR_NUMBER/comments"
+  # Post comment to pull request using GitHub API
+  curl -X POST \
+  -H "Authorization: token $ACCESS_TOKEN" \
+  -d "{\"body\":\"$COMMENT\"}" \
+  "https://api.github.com/repos/$OWNER/$REPO/issues/$PR_NUMBER/comments"
+else
+  echo "No pull request number available"
+fi
