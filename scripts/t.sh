7bce2b413f423ef802f367360dd179c38c2c8aa7
Karl Cardenas <karl@spectrocloud.com>
2023-03-27 18:45:59 -0700

    ci: docs url checker

--- scripts/t.sh
+++ scripts/t.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# set -x
+
+# Set the GitHub API endpoint URL
+GITHUB_API_URL="https://api.github.com"
+
+# Set the pull request number and repository owner and name based on the environment variables
+ACCESS_TOKEN=
+OWNER=spectrocloud
+REPO=librarium
+PR_NUMBER=1209
 
 # Read the JSON file
 json=$(cat link_report.json)
@@ -15,5 +25,12 @@ for link in $(echo "${json}" | jq -r '.links[] | @base64'); do
     COMMENT="${COMMENT}\n\n:link: [${url}](${url})  \n:traffic_light: Status: ${status}  \n:bookmark_tabs: State: ${state}  \n:arrow_up: Parent: ${parent}\n---"
 done
 
-# Output the COMMENT variable in Markdown format to the comment file
-echo -e "${COMMENT}" > comment.md
+# Post the COMMENT variable to the pull request as a comment
+RESPONSE=$(curl -sSL -H "Authorization: token ${ACCESS_TOKEN}" -H "Content-Type: application/json" -d "{\"body\":\"${COMMENT}\"}" -X POST "${GITHUB_API_URL}/repos/${OWNER}/${REPO}/issues/${PR_NUMBER}/comments")
+
+# Check if the response contains an error message
+if [[ "$RESPONSE" == *"message"* ]]; then
+  echo "Error posting comment to pull request"
+  echo "$RESPONSE"
+  exit 1
+fi
