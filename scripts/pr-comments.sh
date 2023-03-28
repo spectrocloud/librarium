54a081754ed9c98c8aed5f11bb685b0fb5406ce5
Karl Cardenas <karl@spectrocloud.com>
2023-03-28 08:10:21 -0700

    ci: docs url checker

--- scripts/pr-comments.sh
+++ scripts/pr-comments.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Set variables for GitHub API
+# Set variables for GitHub API and Slack webhook
 ACCESS_TOKEN=$ACCESS_TOKEN
 OWNER=$OWNER
 REPO=$REPO
+SLACK_WEBHOOK_URL=$SLACK_WEBHOOK_URL
 
 # Define emoji for each object
 EMOJI1=":loudspeaker:"
@@ -46,6 +47,9 @@ for link in $(echo "${JSON_CONTENT}" | jq -r '.links[] | @base64'); do
     COMMENT="${COMMENT}\n\n:link: Broken URL: [${url}](${url})  \n:traffic_light: Status: ${status}  \n:bookmark_tabs: State: ${state}  \n:arrow_up: Parent Page: ${parent}\n---"
 done
 
+# Post the comment to the Slack webhook
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"${COMMENT}\"}" $SLACK_WEBHOOK_URL
+
 if [[ "${DEBUG}" == "true" ]]; then
     echo "GitHub API URL: ${GITHUB_API_URL}"
     echo "Pull Request Number: ${PR_NUMBER}"
@@ -57,16 +61,4 @@ if [[ "${DEBUG}" == "true" ]]; then
     echo "$COMMENT"
 fi
 
-echo "Comment: $COMMENT"
-
-echo "Posting comment to pull request #$PR_NUMBER"
-
-# Post comment to pull request using GitHub API
-RESPONSE=$(curl -sSL -H "Authorization: token ${ACCESS_TOKEN}" -H "Content-Type: application/json" -d "{\"body\":\"${COMMENT}\"}" -X POST "${GITHUB_API_URL}/repos/${OWNER}/${REPO}/issues/${PR_NUMBER}/comments")
-
-# Check if the response contains an error message
-if [[ "$RESPONSE" == *"message"* ]]; then
-  echo "Error posting comment to pull request"
-  echo "$RESPONSE"
-  exit 1
-fi
+echo "Posting comment to pull request #$PR_NUMBER to Slack webhook"
