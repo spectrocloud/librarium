a1020a36ca678d2e0a21d091e93a98485d8c3cd8
Karl Cardenas <karl@spectrocloud.com>
2023-05-26 17:55:15 -0700

    chore: code touchup

--- src/shared/mdx/components/codeBlock.js
+++ src/shared/mdx/components/codeBlock.js
@@ -26,6 +26,7 @@ const LoadableComponent = Loadable({
 
 /* eslint-disable react/jsx-key */
 const CodeBlock = ({ children: exampleCode, ...props }) => {
+  const lineHighlightDefaultColor = '#666666'
   const language = props?.className?.split("language-")?.[1] || "json";
   if (props["react-live"]) {
     return <LoadableComponent code={exampleCode} />;
@@ -35,10 +36,9 @@ const CodeBlock = ({ children: exampleCode, ...props }) => {
     if (props.coloredLines) {
       const iterations = props.coloredLines.split(",");
       iterations.forEach((iteration) => {
-        const [range, color] = iteration.split("|");
-        coloredIntervals[range] = color;
-        // set color to have a default value of #666666 regardless of whether it's provided. This is to ensure that the all color highlights used throughout the site are consistent.
-        coloredIntervals[range] = "#666666";
+        const range = iteration.split("|")[0];
+        // Always set color to #666666, regardless of whether "|" exists. This is to ensure consitency.
+        coloredIntervals[range] = lineHighlightDefaultColor;
       });
     }
 
