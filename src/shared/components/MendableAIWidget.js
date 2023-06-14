520f6b1171d750d239d2ca6b45638355cf2dd3ea
Nageshwara sairam <113401242+nagesh161007@users.noreply.github.com>
2023-06-14 10:28:57 -0400

    chore(mendable-ai-integration): move it to seperate component

---
+++ src/shared/components/MendableAIWidget.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import spectrocloudLogo from "assets/spectrocloud-logo.png";
+export const MENDABLE_SCRIPT_URL =
+  "https://unpkg.com/@mendable/search@0.0.109-beta.9/dist/umd/mendable-bundle.min.js";
+
+export default function MendableAIWidget() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = MENDABLE_SCRIPT_URL;
+    script.async = true;
+
+    document.body.appendChild(script);
+
+    script.onload = () => {
+      if (window.Mendable) {
+        window.Mendable.initialize({
+          anon_key: process.env.GATSBY_MENDABLE_API_KEY,
+          type: "floatingButton",
+          dialogPlaceholder: "What is Palette?",
+          floatingButtonStyle: {
+            color: "#fff",
+            backgroundColor: "#3E4FB5",
+          },
+          icon: spectrocloudLogo,
+          style: { accentColor: "#3E4FB5" },
+        });
+      }
+    };
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
+}
--- src/shared/layouts/Persistent/index.js
+++ src/shared/layouts/Persistent/index.js
@@ -1,23 +1,13 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Provider from "./provider";
-import spectrocloudLogo from "assets/spectrocloud-logo.png";
+import MendableAIWidget from "../../components/MendableAIWidget";
 
 const Persistent = ({ children }) => {
-  useEffect(() => {
-    window.Mendable &&
-      window.Mendable.initialize({
-        anon_key: process.env.GATSBY_MENDABLE_API_KEY,
-        type: "floatingButton",
-        dialogPlaceholder: "What is Palette?",
-        floatingButtonStyle: {
-          color: "#fff",
-          backgroundColor: "#3E4FB5",
-        },
-        icon: spectrocloudLogo,
-        style: { accentColor: "#3E4FB5" },
-      });
-  }, []);
-
-  return <Provider>{children}</Provider>;
+  return (
+    <Provider>
+      <MendableAIWidget />
+      {children}
+    </Provider>
+  );
 };
 export default Persistent;
