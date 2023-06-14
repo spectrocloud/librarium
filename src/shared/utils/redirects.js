8af223092bf7356ed9664cb04aa1e9d43949e978
Karl Cardenas <karl@spectrocloud.com>
2023-06-14 10:46:20 -0700

    chore: added rbac image

--- src/shared/utils/redirects.js
+++ src/shared/utils/redirects.js
@@ -238,12 +238,6 @@ const redirects = [
     isPermanent: true,
   },
   {
-    fromPath: `/clusters/edge/edgeforge-workflow/build-content-bundle`,
-    toPath: `/clusters/edge/edgeforge-workflow/palette-canvos`,
-    redirectInBrowser: true,
-    isPermanent: true,
-  },
-  {
     fromPath: `/clusters/edge/edgeforge-workflow/build-images`,
     toPath: `/clusters/edge/edgeforge-workflow/palette-canvos`,
     redirectInBrowser: true,
