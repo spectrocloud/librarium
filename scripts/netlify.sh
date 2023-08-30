9444f9fe734c20bfd1c29b66e0cc260f2f4db298
Karl Cardenas <karl@spectrocloud.com>
2023-08-30 10:17:24 -0700

    ci: added logic for backport PRs (#1501)
    * ci: added logic for backport PRs
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * ci: fixed backport yaml issue
    
    * docs: updated README with Netlify steps
    
    (cherry picked from commit 913a1180fa1c45288475f9f687a91efe3c901b24)

---
+++ scripts/netlify.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# List of branches to NOT create a Netlify preview
+# Master branch does not need a preview
+# Release branches get a preview through docs-latest.spectrocloud.com
+allowed_branches=("master" "release-*")
+
+echo "Branch name: $HEAD"
+
+# Read the environment variable
+branch_name=$HEAD
+
+# Initialize a flag to check if the branch is not allowed
+not_allowed=0
+
+# Compare $HEAD against the list
+for allowed in "${allowed_branches[@]}"
+do
+  # Use double brackets and == for pattern matching
+  if [[ "$branch_name" == $allowed ]]; then
+    not_allowed=1
+    break
+  fi
+done
+
+# Exit accordingly
+if [ $not_allowed -eq 1 ]; then
+  echo "Not allowed to create a Netlify preview"
+  exit 0
+else
+  echo "Allowed to create a Netlify preview"
+  exit 1
+fi
