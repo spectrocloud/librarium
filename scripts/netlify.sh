de5d43157024c5b4320616948f83c341f309422e
Karl Cardenas <karl@spectrocloud.com>
2023-08-30 09:07:49 -0700

    ci: fixed backport yaml issue

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-[ "$NETLIFY_PREVIEW" == "true" ] && exit 1 || exit 0
+# List of branches to NOT create a Netlify preview
+allowed_branches=("master" "version-*", "release-*")
+
+# Read the environment variable
+branch_name=$BRANCH
+
+# Initialize a flag to check if the branch is not allowed
+not_allowed=0
+
+# Compare $BRANCH against the list
+for allowed in "${allowed_branches[@]}"
+do
+  # Use double brackets and == for pattern matching
+  if [[ "$branch_name" == $allowed ]]; then
+    not_allowed=1
+    break
+  fi
+done
+
+# Exit accordingly
+if [ $not_allowed -eq 1 ]; then
+  exit 0
+else
+  exit 1
+fi
