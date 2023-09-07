4a80d3b18ef9edc7d38d108bddb9f5cc9f4a6ce5
Karl Cardenas <karl@spectrocloud.com>
2023-09-07 13:13:03 -0700

    ci: added script to update docusarus config file programmatically

---
+++ scripts/update_docusarus_config.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+const parser = require('@babel/parser');
+const generate = require('@babel/generator').default;
+const t = require('@babel/types');
+
+let versionsArray = [];
+try {
+  const versionsJson = fs.readFileSync('versions.json', 'utf8');
+  versionsArray = JSON.parse(versionsJson);
+} catch (err) {
+  console.error('Could not read versions.json:', err);
+}
+
+// Read and parse the existing docusaurus.config.js
+const configCode = fs.readFileSync('docusaurus.config.js', 'utf8');
+const ast = parser.parse(configCode, {
+  sourceType: 'module',
+});
+
+// Find the 'config' object
+let configObject;
+ast.program.body.forEach((node) => {
+  if (
+    node.type === 'VariableDeclaration' &&
+    node.declarations[0].id.name === 'config'
+  ) {
+    configObject = node.declarations[0].init.properties.find(
+      (prop) => prop.key.name === 'presets'
+    ).value.elements[0].elements[1].properties.find(
+      (prop) => prop.key.name === 'docs'
+    ).value.properties.find(
+      (prop) => prop.key.name === 'versions'
+    ).value;
+  }
+});
+
+// Add versions to 'configObject'
+versionsArray.forEach((version) => {
+  const versionProperty = t.objectProperty(
+    t.stringLiteral(version),
+    // Add the 'banner' property to the version object and set it to 'none'
+    // This removes the warning banner from the versioned docs that states a version is not maintained anymore.
+    t.objectExpression([t.objectProperty(t.identifier('banner'), t.stringLiteral('none'))])
+  );
+  configObject.properties.push(versionProperty);
+});
+
+// Generate the updated code
+const updatedCode = generate(ast).code;
+
+// Write the updated code back to docusaurus.config.js
+fs.writeFileSync('temp.docusaurus.config.js', updatedCode);
