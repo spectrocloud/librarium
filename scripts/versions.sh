7ca649fb84ef08134b6235ef5026c1e8269540a7
Karl Cardenas <karl@spectrocloud.com>
2023-09-07 18:59:15 -0700

    save

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -12,105 +12,105 @@
 ###################################################################################################                      
 
 tempdir=$1
-baseDir=$($PWD) # Get the parent directory of the current directory
+baseDir="$PWD" # Get the parent directory of the current directory
 
 echo "Temp directory: $tempdir"
 echo "Base directory: $baseDir"
-# # List of version branches to exclude
-# exclude_branches=("version-3-4") # DO NOT ADD A COMMA BETWEEN THE BRANCHES. ADD A SPACE INSTEAD AND THE NEW VERSION STRING.
-# # exclude_branches=("version-3-4")
+# List of version branches to exclude
+exclude_branches=("version-3-4") # DO NOT ADD A COMMA BETWEEN THE BRANCHES. ADD A SPACE INSTEAD AND THE NEW VERSION STRING.
+# exclude_branches=("version-3-4")
 
-# # Save the current branch name
-# current_branch=$(git branch --show-current)
+# Save the current branch name
+current_branch=$(git branch --show-current)
 
-# # Fetch all branches
-# git fetch
+# Fetch all branches
+git fetch
 
-# # Remove the existing versioned directories in the temp directory.
-# rm -rf $tempdir/staging_docs
-# rm -rf $tempdir/staging_sidebars
-# rm -rf $tempdir/temp_versions.json
+# Remove the existing versioned directories in the temp directory.
+rm -rf $tempdir/staging_docs
+rm -rf $tempdir/staging_sidebars
+rm -rf $tempdir/temp_versions.json
 
 
-# # Create the staging directory for all the versions
-# mkdir -p $tempdir/staging_docs
-# mkdir -p $tempdir/staging_sidebars
-# touch $tempdir/temp_versions.json
-# echo '[]' > $tempdir/temp_versions.json  # Initialize as an empty array if it doesn't exist
+# Create the staging directory for all the versions
+mkdir -p $tempdir/staging_docs
+mkdir -p $tempdir/staging_sidebars
+touch $tempdir/temp_versions.json
+echo '[]' > $tempdir/temp_versions.json  # Initialize as an empty array if it doesn't exist
 
-# # Loop through all local branches
-# for branch in $(git branch --format '%(refname:short)'); do
+# Loop through all local branches
+for branch in $(git branch --format '%(refname:short)'); do
   
-#   # Check if the branch is in the exclude list
-#   if [[ " ${exclude_branches[@]} " =~ " ${branch} " ]]; then
-#     echo "Skipping excluded branch: $branch"
-#     continue
-#   fi
+  # Check if the branch is in the exclude list
+  if [[ " ${exclude_branches[@]} " =~ " ${branch} " ]]; then
+    echo "Skipping excluded branch: $branch"
+    continue
+  fi
   
-#   # Check if the branch name starts with 'version-' followed by numbers
-#   if [[ $branch =~ ^version-[0-9]+(-[0-9]+)*$ ]]; then
-#     echo "Found branch: $branch"
+  # Check if the branch name starts with 'version-' followed by numbers
+  if [[ $branch =~ ^version-[0-9]+(-[0-9]+)*$ ]]; then
+    echo "Found branch: $branch"
 
-#     # Extract the version and replace '-' with '.'
-#     version=${branch#version-}
-#     version=${version//-/.}
+    # Extract the version and replace '-' with '.'
+    version=${branch#version-}
+    version=${version//-/.}
 
-#     # Append .0 to the version
-#     version="$version.0"
+    # Append .0 to the version
+    version="$version.0"
 
-#     # Store in a variable
-#     extracted_version=$version
-#     echo "Extracted version: $extracted_version"
+    # Store in a variable
+    extracted_version=$version
+    echo "Extracted version: $extracted_version"
 
-#     # Add version to temp_versions.json and sort it
-#     jq --arg ver "$extracted_version" '. |= [$ver] + . | sort_by(. | split(".") | map(tonumber)) | reverse' $tempdir/temp_versions.json > $tempdir/temp.json && mv $tempdir/temp.json $tempdir/temp_versions.json && rm $tempdir/temp.json
+    # Add version to temp_versions.json and sort it
+    jq --arg ver "$extracted_version" '. |= [$ver] + . | sort_by(. | split(".") | map(tonumber)) | reverse' $tempdir/temp_versions.json > $tempdir/temp.json && mv $tempdir/temp.json $tempdir/temp_versions.json && rm $tempdir/temp.json
 
-#     # Switch to the version branch
-#     git checkout $branch
+    # Switch to the version branch
+    git checkout $branch
 
-#     # Pull the latest changes 
-#     git pull origin $branch
+    # Pull the latest changes 
+    git pull origin $branch
 
-#     # Run the npm command
-#     echo "Running: npm run docusaurus docs:version $extracted_version"
-#     npm run docusaurus docs:version $extracted_version
+    # Run the npm command
+    echo "Running: npm run docusaurus docs:version $extracted_version"
+    npm run docusaurus docs:version $extracted_version
 
 
-#     # Copy the generated files to the staging directory
-#     echo "Copying files to staging directory"
-#     mkdir -p $tempdir/staging_docs/version-$extracted_version
-#     mkdir -p $tempdir/staging_sidebars/version-$extracted_version
+    # Copy the generated files to the staging directory
+    echo "Copying files to staging directory"
+    mkdir -p $tempdir/staging_docs/version-$extracted_version
+    mkdir -p $tempdir/staging_sidebars/version-$extracted_version
     
-#     cp -R versioned_docs/version-$extracted_version $tempdir/staging_docs/version-$extracted_version
-#     cp -R versioned_sidebars/version-$extracted_version $tempdir/staging_sidebars/version-$extracted_version
-#     cp versioned_sidebars/version-$extracted_version-sidebars.json $tempdir/staging_sidebars/version-$extracted_version-sidebars.json
+    cp -R versioned_docs/version-$extracted_version $tempdir/staging_docs/version-$extracted_version
+    cp -R versioned_sidebars/version-$extracted_version $tempdir/staging_sidebars/version-$extracted_version
+    cp versioned_sidebars/version-$extracted_version-sidebars.json $tempdir/staging_sidebars/version-$extracted_version-sidebars.json
 
 
-#     rm -rf versioned_docs/
-#     rm -rf versioned_sidebars/
+    rm -rf versioned_docs/
+    rm -rf versioned_sidebars/
 
-#     rm versions.json
+    rm versions.json
 
-#     # Switch back to the original branch
-#     git checkout $current_branch
-#   fi
-# done
+    # Switch back to the original branch
+    git checkout $current_branch
+  fi
+done
 
-# # Rename the staging directory to the expected Docusarus versioned directory names
-# cp -R $tempdir/staging_docs $baseDir/versioned_docs
-# cp -R $tempdir/staging_sidebars $baseDir/versioned_sidebars
+# Rename the staging directory to the expected Docusarus versioned directory names
+cp -R $tempdir/staging_docs $baseDir/versioned_docs
+cp -R $tempdir/staging_sidebars $baseDir/versioned_sidebars
 
-# # Remove the existing versions.json if it exists
-# [ -e versions.json ] && rm versions.json
+# Remove the existing versions.json if it exists
+[ -e versions.json ] && rm versions.json
 
-# # Rename temp_versions.json to versions.json
-# mv $tempdir/temp_versions.json $baseDir/versions.json
+# Rename temp_versions.json to versions.json
+mv $tempdir/temp_versions.json $baseDir/versions.json
 
-# # node $baseDir/scripts/update_docusarus_config.js $tempdir $baseDir
+# node $baseDir/scripts/update_docusarus_config.js $tempdir $baseDir
 
-# # if [ $? -ne 0 ]; then
-# #   echo "Error updating docusarus.config.js"
-# #   exit 1
-# # fi
+# if [ $? -ne 0 ]; then
+#   echo "Error updating docusarus.config.js"
+#   exit 1
+# fi
 
-# mv $tempdir/temp.docusaurus.config.js $baseDir/docusaurus.config.js
+mv $tempdir/temp.docusaurus.config.js $baseDir/docusaurus.config.js
