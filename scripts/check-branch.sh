a418dc568d0ab89165d47d3f0803ec0eadd64c2c
Karl Cardenas <karl@spectrocloud.com>
2023-09-12 13:28:48 -0700

    ci: more netlify changes

--- scripts/check-branch.sh
+++ scripts/check-branch.sh
@@ -8,8 +8,10 @@ do
     branch=$(echo $local_ref | sed 's!refs/heads/!!')
 
     if [[ $branch == version-* ]]; then
-      echo "Branch names starting with 'version-' are not allowed."
+      echo "🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫"
+      echo "Creating branch names starting with 'version-*' are not allowed."
       echo "If you need to override for version branch creation, use the ---no-verify flag."
+      echo "Ask the team if you need help or if you are unsure."
       exit 1
     fi
   fi
