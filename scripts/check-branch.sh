f41a53bfbaedf7987a112e96035383af2f4bcefd
Karl Cardenas <karl@spectrocloud.com>
2023-09-12 13:42:44 -0700

    ci: more netlify changes

--- scripts/check-branch.sh
+++ scripts/check-branch.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
 
-zero_commit="0000000000000000000000000000000000000000"
 
 while read local_ref local_sha remote_ref remote_sha
 do
-  if [ "$remote_sha" = "$zero_commit" ]; then
-    branch=$(echo $local_ref | sed 's!refs/heads/!!')
-
-    if [[ $branch == version-* ]]; then
-      echo "🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫"
-      echo "Creating branch names starting with 'version-*' are not allowed."
-      echo "If you need to override for version branch creation, use the ---no-verify flag."
-      echo "Ask the team if you need help or if you are unsure."
-      exit 1
-    fi
+  branch=$(echo $local_ref | sed 's!refs/heads/!!')
+  
+  if [[ $branch == version-* ]]; then
+    echo "🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫🚫"
+    echo "Creating branch names starting with 'version-*' are not allowed."
+    echo "If you need to override for version branch creation, use the --no-verify flag."
+    echo "Ask the team if you need help or if you are unsure."
+    exit 1
   fi
 done
