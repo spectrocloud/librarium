56605ad7c1a035f3d30571f4f811465badcaf952
Karl Cardenas <karl@spectrocloud.com>
2024-01-17 09:09:26 -0700

    docs: DOC-1012  ISO 27001 (#2031)
    * docs: updated language and added ISO 27001
    
    * docs: fixed broken anchors
    
    * docs: more finding
    
    * docs: compliance image update
    
    * docs: fixed broken achor
    
    * docs: updated index page
    
    * docs: vale feedback
    
    * Optimised images with calibre/image-actions
    
    * Apply suggestions from code review
    
    Co-authored-by: Lenny Chen <55669665+lennessyy@users.noreply.github.com>
    
    * docs: feedback
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Lenny Chen <55669665+lennessyy@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,6 +25,7 @@ const config = {
   // Usually your repo name.
 
   onBrokenLinks: "throw",
+  onBrokenAnchors: "throw",
   onBrokenMarkdownLinks: "throw",
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
