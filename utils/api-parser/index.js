12cf7979dfd2638533dbe41b4e59b4de86c0c375
Karl Cardenas <karl@spectrocloud.com>
2024-03-05 08:22:39 -0700

    chore: new test

--- utils/api-parser/index.js
+++ utils/api-parser/index.js
@@ -44,7 +44,7 @@ fs.readFile("docs/api-content/api-docs/v1/api.json", "utf8", (err, data) => {
     jsonData.servers = [
       {
         // url: "https://api.spectrocloud.com",
-        url: "https://api.stage.spectrocloud.com",
+        url: "https://stage.spectrocloud.com",
       },
     ];
 
