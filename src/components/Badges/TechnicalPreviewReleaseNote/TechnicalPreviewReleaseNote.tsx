7a7663f04146db5b296bea95b34cfe9030c0d2cb
Karl Cardenas <karl@spectrocloud.com>
2024-03-05 14:02:28 -0700

    docs: ready to implement

--- src/components/Badges/TechnicalPreviewReleaseNote/TechnicalPreviewReleaseNote.tsx
+++ src/components/Badges/TechnicalPreviewReleaseNote/TechnicalPreviewReleaseNote.tsx
@@ -1,11 +1,13 @@
-import ThemedImage from "@theme/ThemedImage";
 import React from "react";
+import ThemedImage from "@theme/ThemedImage";
 import styles from "./TechnicalPreviewReleaseNote.module.scss";
+
+// define type for TechnicalPreviewReleaseNote
 
 export default function TechnicalPreviewReleaseNote() {
   return (
     <ThemedImage
-      alt="Technical preview feature"
+      alt="Technical preview feature badge"
       sources={{
         light: "/img/tech-preview-dark.svg",
         dark: "/img/tech-preview-light.svg",
