babf122cfd04f15064d5550f13113e7f8cd5a8c4
addetz <43963729+addetz@users.noreply.github.com>
2024-03-05 15:18:19 +0000

    docs: reduce duplication in SimpleCard test

--- src/components/SimpleCard/SimpleCard.test.tsx
+++ src/components/SimpleCard/SimpleCard.test.tsx
@@ -10,7 +10,29 @@ describe("Display SimpleCardGrid", () => {
     relativeURL: string;
   }
 
-  function assert(cardsPerRow: number, testCases: testCase[], container: HTMLElement) {
+  const testCases: testCase[] = [
+    {
+      title: "1. First Card",
+      description: "Card 1",
+      buttonText: "Learn more 1",
+      relativeURL: "./getting-started-1",
+    },
+    {
+      title: "2. Second Card",
+      description: "Card 2",
+      buttonText: "Learn more 2",
+      relativeURL: "./getting-started-2",
+    },
+    {
+      title: "3. Third Card",
+      description: "Card 3",
+      buttonText: "Learn more 3",
+      relativeURL: "./getting-started-3",
+    },
+  ];
+
+  function assert(cardsPerRow: number, testCases: testCase[]) {
+    const { container } = render(<SimpleCardGrid cardsPerRow={cardsPerRow} cards={testCases} />);
     testCases.forEach((tc) => {
       expect(screen.getByText(tc.title)).toBeInTheDocument();
       expect(screen.getByText(tc.description)).toBeInTheDocument();
@@ -28,64 +50,19 @@ describe("Display SimpleCardGrid", () => {
 
   it("multiple row cards", () => {
     const cardsPerRow: number = 2;
-    const testCases: testCase[] = [
-      {
-        title: "1. First Card",
-        description: "Card 1",
-        buttonText: "Learn more 1",
-        relativeURL: "./getting-started-1",
-      },
-      {
-        title: "2. Second Card",
-        description: "Card 2",
-        buttonText: "Learn more 2",
-        relativeURL: "./getting-started-2",
-      },
-      {
-        title: "3. Third Card",
-        description: "Card 3",
-        buttonText: "Learn more 3",
-        relativeURL: "./getting-started-3",
-      },
-    ];
-
-    const { container } = render(<SimpleCardGrid cardsPerRow={cardsPerRow} cards={testCases} />);
-    assert(cardsPerRow, testCases, container);
+    const tcs: testCase[] = testCases;
+    assert(cardsPerRow, tcs);
   });
 
   it("single row cards", () => {
     const cardsPerRow: number = 2;
-    const testCases: testCase[] = [
-      {
-        title: "1. First Card",
-        description: "Card 1",
-        buttonText: "Learn more 1",
-        relativeURL: "./getting-started-1",
-      },
-      {
-        title: "2. Second Card",
-        description: "Card 2",
-        buttonText: "Learn more 2",
-        relativeURL: "./getting-started-2",
-      },
-    ];
-
-    const { container } = render(<SimpleCardGrid cardsPerRow={2} cards={testCases} />);
-    assert(cardsPerRow, testCases, container);
+    const tcs: testCase[] = testCases.slice(2);
+    assert(cardsPerRow, tcs);
   });
 
   it("less cards than a single row", () => {
     const cardsPerRow: number = 2;
-    const testCases: testCase[] = [
-      {
-        title: "1. First Card",
-        description: "Card 1",
-        buttonText: "Learn more 1",
-        relativeURL: "./getting-started-1",
-      },
-    ];
-
-    const { container } = render(<SimpleCardGrid cardsPerRow={2} cards={testCases} />);
-    assert(cardsPerRow, testCases, container);
+    const tcs: testCase[] = testCases.slice(1);
+    assert(cardsPerRow, tcs);
   });
 });
