00faf96a5437789f5d0aec5a8debc2591fb3e57d
addetz <43963729+addetz@users.noreply.github.com>
2024-03-05 14:20:33 +0000

    docs: refactor grid test DOC-1067

--- src/components/SimpleCard/SimpleCard.test.tsx
+++ src/components/SimpleCard/SimpleCard.test.tsx
@@ -1,101 +1,91 @@
 import React from "react";
-import { render, waitFor, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import SimpleCardGrid from ".";
 
 describe("Display SimpleCardGrid", () => {
-  it("multiple row cards", () => {
-    const { container } = render(
-      <SimpleCardGrid
-        cardsPerRow={2}
-        cards={[
-          {
-            title: "1. First Card",
-            description: "Card 1",
-            buttonText: "Learn more",
-            relativeURL: "./getting-started",
-          },
-          {
-            title: "2. Second Card",
-            description: "Card 2",
-            buttonText: "Learn more",
-            relativeURL: "./getting-started",
-          },
-          {
-            title: "3. Third Card",
-            description: "Card 3",
-            buttonText: "Learn more",
-            relativeURL: "./getting-started",
-          },
-        ]}
-      />
-    );
+  interface testCase {
+    title: string;
+    description: string;
+    buttonText: string;
+    relativeURL: string;
+  }
 
-    expect(screen.getByText(/1. First Card/i)).toBeInTheDocument();
-    expect(screen.getByText(/Card 1/i)).toBeInTheDocument();
-    expect(screen.getByText(/2. Second Card/i)).toBeInTheDocument();
-    expect(screen.getByText(/Card 2/i)).toBeInTheDocument();
-    expect(screen.getByText(/3. Third Card/i)).toBeInTheDocument();
-    expect(screen.getByText(/Card 3/i)).toBeInTheDocument();
-    expect(screen.getAllByRole("button")).toHaveLength(3);
-    screen.getAllByRole("button").forEach((btn) => {
-      expect(btn).toHaveTextContent(/Learn More/i);
+  function assert(cardsPerRow: number, testCases: testCase[], container: HTMLElement) {
+    testCases.forEach((tc) => {
+      expect(screen.getByText(tc.title)).toBeInTheDocument();
+      expect(screen.getByText(tc.description)).toBeInTheDocument();
+      expect(screen.getByText(tc.buttonText, { selector: "button" })).toBeInTheDocument();
+      expect(
+        screen.getAllByRole("link").filter((value) => {
+          return value.getAttribute("href") == tc.relativeURL;
+        })
+      ).not.toBeNull();
     });
-    expect(container.querySelectorAll(".row")).toHaveLength(2);
+
+    expect(screen.getAllByRole("button")).toHaveLength(testCases.length);
+    expect(container.querySelectorAll(".row")).toHaveLength(Math.ceil(testCases.length / cardsPerRow));
+  }
+
+  it("multiple row cards", () => {
+    const cardsPerRow: number = 2;
+    const testCases: testCase[] = [
+      {
+        title: "1. First Card",
+        description: "Card 1",
+        buttonText: "Learn more 1",
+        relativeURL: "./getting-started-1",
+      },
+      {
+        title: "2. Second Card",
+        description: "Card 2",
+        buttonText: "Learn more 2",
+        relativeURL: "./getting-started-2",
+      },
+      {
+        title: "3. Third Card",
+        description: "Card 3",
+        buttonText: "Learn more 3",
+        relativeURL: "./getting-started-3",
+      },
+    ];
+
+    const { container } = render(<SimpleCardGrid cardsPerRow={cardsPerRow} cards={testCases} />);
+    assert(cardsPerRow, testCases, container);
   });
 
   it("single row cards", () => {
-    const { container } = render(
-      <SimpleCardGrid
-        cardsPerRow={2}
-        cards={[
-          {
-            title: "1. First Card",
-            description: "Card 1",
-            buttonText: "Learn more",
-            relativeURL: "./getting-started",
-          },
-          {
-            title: "2. Second Card",
-            description: "Card 2",
-            buttonText: "Learn more",
-            relativeURL: "./getting-started",
-          },
-        ]}
-      />
-    );
+    const cardsPerRow: number = 2;
+    const testCases: testCase[] = [
+      {
+        title: "1. First Card",
+        description: "Card 1",
+        buttonText: "Learn more 1",
+        relativeURL: "./getting-started-1",
+      },
+      {
+        title: "2. Second Card",
+        description: "Card 2",
+        buttonText: "Learn more 2",
+        relativeURL: "./getting-started-2",
+      },
+    ];
 
-    expect(screen.getByText(/1. First Card/i)).toBeInTheDocument();
-    expect(screen.getByText(/Card 1/i)).toBeInTheDocument();
-    expect(screen.getByText(/2. Second Card/i)).toBeInTheDocument();
-    expect(screen.getByText(/Card 2/i)).toBeInTheDocument();
-    expect(screen.getAllByRole("button")).toHaveLength(2);
-    screen.getAllByRole("button").forEach((btn) => {
-      expect(btn).toHaveTextContent(/Learn More/i);
-    });
-    expect(container.querySelectorAll(".row")).toHaveLength(1);
+    const { container } = render(<SimpleCardGrid cardsPerRow={2} cards={testCases} />);
+    assert(cardsPerRow, testCases, container);
   });
 
   it("less cards than a single row", () => {
-    const { container } = render(
-      <SimpleCardGrid
-        cardsPerRow={2}
-        cards={[
-          {
-            title: "1. First Card",
-            description: "Card 1",
-            buttonText: "Learn more",
-            relativeURL: "./getting-started",
-          },
-        ]}
-      />
-    );
+    const cardsPerRow: number = 2;
+    const testCases: testCase[] = [
+      {
+        title: "1. First Card",
+        description: "Card 1",
+        buttonText: "Learn more 1",
+        relativeURL: "./getting-started-1",
+      },
+    ];
 
-    expect(screen.getByText(/1. First Card/i)).toBeInTheDocument();
-    expect(screen.getByText(/Card 1/i)).toBeInTheDocument();
-    expect(screen.getAllByRole("button")).toHaveLength(1);
-    screen.getAllByRole("button").forEach((btn) => {
-      expect(btn).toHaveTextContent(/Learn More/i);
-    });
-    expect(container.querySelectorAll(".row")).toHaveLength(1);
+    const { container } = render(<SimpleCardGrid cardsPerRow={2} cards={testCases} />);
+    assert(cardsPerRow, testCases, container);
   });
 });
