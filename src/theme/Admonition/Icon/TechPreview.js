5d747c505894226f5c3f9334bf15705fd3e2e40a
Yuliia Horbenko <31223054+yuliiiah@users.noreply.github.com>
2024-03-07 21:24:45 +0100

    Update the TechPreview component to also accept custom text (#2377)
    * chore: Update the TechPreview component to also accept custom text
    
    * chore: Fix the linter warning and clean up CSS a bit

--- src/theme/Admonition/Icon/TechPreview.js
+++ src/theme/Admonition/Icon/TechPreview.js
@@ -2,6 +2,6 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFlag } from "@fortawesome/free-solid-svg-icons";
 
-export default function IconTechPreview(props) {
+export default function IconTechPreview() {
   return <FontAwesomeIcon icon={faFlag} />;
 }
