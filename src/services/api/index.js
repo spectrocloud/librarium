920c8a584de5f2ac7625718ef3e426fe0c9e3ee3
nage1234 <88926279+nage1234@users.noreply.github.com>
2024-03-26 19:48:56 +0530

    initial commit

---
+++ src/services/api/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
+
+const api = axios.create({
+  baseURL: 'https://dev.spectrocloud.com',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+    "ApiKey": "NGU5MTNhNjBhNzM4OWI1ZTc0N2M5ZmQ0NDI1ZTk4ZTc=",
+  }
+});
+
+axiosRetry(api, {
+  retries: 3,
+  retryDelay: (...arg) => axiosRetry.exponentialDelay(...arg, 1000),
+  retryCondition(error) {
+    switch (error.response.status) {
+      case 500:
+      case 501:
+      case 429:
+        return true;
+      default:
+        return false;
+    }
+  },
+});
+
+
+module.exports = api;
