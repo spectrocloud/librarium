f8b7b6bc92debe829940030a3cd0399f84c1c644
Karl Cardenas <karl@spectrocloud.com>
2024-03-26 18:31:11 -0700

    chore: test preview

--- src/components/ReleaseNotesVersions/ReleaseNotesVersions.tsx
+++ src/components/ReleaseNotesVersions/ReleaseNotesVersions.tsx
@@ -83,7 +83,7 @@ export default function ReleaseNotesVersions(): JSX.Element {
           </option>
           {versions.map((version: Version) => (
             <option key={version.name} value={version.name}>
-              {version.url.startsWith("http") ? version.name + " (External)" : version.name}
+              {version.url.startsWith("http") ? version.name + " ↗️" : version.name}
             </option>
           ))}
         </select>
