b7e0248ba8527d04438265d74aeb40be180ba2ed
Karl Cardenas <karl@spectrocloud.com>
2024-03-29 08:41:32 -0700

    ci: testing visual comp

---
+++ tests/screenshot.spec.ts
@@ -0,0 +1,37 @@
+import * as fs from "fs";
+import { test } from "@playwright/test";
+import { argosScreenshot } from "@argos-ci/playwright";
+import { extractSitemapPathnames, pathnameToArgosName } from "argos/utils";
+
+// Constants:
+const siteUrl = "http://localhost:3000";
+const sitemapPath = "../website/build/sitemap.xml";
+const stylesheetPath = "./screenshot.css";
+const stylesheet = fs.readFileSync(stylesheetPath).toString();
+
+// Wait for hydration, requires Docusaurus v2.4.3+
+// See https://github.com/facebook/docusaurus/pull/9256
+// Docusaurus adds a <html data-has-hydrated="true"> once hydrated
+function waitForDocusaurusHydration() {
+  return document.documentElement.dataset.hasHydrated === "true";
+}
+
+function isVersionedDocsPathname(pathname: string): boolean {
+  return pathname.match(/^\/docs\/((\d\.\d\.\d)|(next))\//);
+}
+
+function screenshotPathname(pathname: string) {
+  test(`pathname ${pathname}`, async ({ page }) => {
+    const url = siteUrl + pathname;
+    await page.goto(url);
+    await page.waitForFunction(waitForDocusaurusHydration);
+    await page.addStyleTag({ content: stylesheet });
+    await argosScreenshot(page, pathnameToArgosName(pathname));
+  });
+}
+
+test.describe("Docusaurus site screenshots", () => {
+  const pathnames = extractSitemapPathnames(sitemapPath).filter(isVersionedDocsPathname);
+  console.log("Pathnames to screenshot:", pathnames);
+  pathnames.forEach(screenshotPathname);
+});
