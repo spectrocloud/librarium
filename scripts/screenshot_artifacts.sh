b05d5c216e6845726b9194717ac028b2efe594cc
Karl Cardenas <karl@spectrocloud.com>
2024-04-01 17:10:31 -0700

    ci: updated artifacts logic

--- scripts/screenshot_artifacts.sh
+++ scripts/screenshot_artifacts.sh
@@ -10,7 +10,6 @@
 # Otherwise the artifacts cannot be downloaded.
 ####################################################################################################
 
-
 set -e
 
 DESTINATION_FOLDER=$1
@@ -33,13 +32,11 @@ if [ -z "$GITHUB_TOKEN" ]; then
   exit 1
 fi
 
-
 OWNER="${GITHUB_REPOSITORY_OWNER:-spectrocloud}"
 REPO_NAME="${GITHUB_REPOSITORY#*/}"  
 REPO="${REPO_NAME:-librarium}"
 WORKFLOW_FILE="screenshot_capture.yaml"
 UNZIP_FOLDER="${DESTINATION_FOLDER:-screenshots}" 
-
 
 perform_curl() {
     local url=$1
@@ -65,52 +62,47 @@ ARTIFACTS_URL=$(echo $LATEST_RUN | jq -r '.artifacts_url')
 
 # Check if the status is "completed" and the conclusion is "success"
 if [[ "$STATUS" == "completed" && "$CONCLUSION" == "success" ]]; then
-    echo "The latest workflow run is completed successfully."
+    echo "The latest workflow run completed successfully ✅"
 
     # Fetch artifacts details
     ARTIFACTS=$(perform_curl "$ARTIFACTS_URL")
 
-    # Parse and display artifact information
-    ARTIFACTS_COUNT=$(echo $ARTIFACTS | jq '.total_count')
+    # Display total count of artifacts
+    ARTIFACTS_COUNT=$(echo "$ARTIFACTS" | jq '.total_count')
     echo "Found $ARTIFACTS_COUNT artifact(s)."
-  
+    
     if [[ "$ARTIFACTS_COUNT" -gt 0 ]]; then
         # Loop through each artifact and print details
         echo $ARTIFACTS | jq -c '.artifacts[]' | while read -r ARTIFACT; do
             NAME=$(echo $ARTIFACT | jq -r '.name')
-            DOWNLOAD_URL=$(echo $ARTIFACT | jq -r '.archive_download_url')
             SIZE=$(echo $ARTIFACT | jq -r '.size_in_bytes')
             CREATED_AT=$(echo $ARTIFACT | jq -r '.created_at')
             EXPIRES_AT=$(echo $ARTIFACT | jq -r '.expires_at')
-
             echo "Artifact: $NAME"
             echo "Size: $SIZE bytes"
             echo "Created at: $CREATED_AT"
             echo "Expires at: $EXPIRES_AT"
-            echo "Download URL: $DOWNLOAD_URL"
             echo "--------------------------------"
         done
 
-        # Download the first artifact
-        DOWNLOAD_URL=$(echo $ARTIFACTS | jq -r '.artifacts[0].archive_download_url')
-
-        if [ ! -z "$DOWNLOAD_URL" ] && [ "$DOWNLOAD_URL" != "null" ]; then
+        # Specifically download the "screenshots" artifact
+        DOWNLOAD_URL=$(echo "$ARTIFACTS" | jq -r '.artifacts[] | select(.name == "screenshots") | .archive_download_url')
+        if [[ -n "$DOWNLOAD_URL" && "$DOWNLOAD_URL" != "null" ]]; then
             DOWNLOAD_PATH="artifact.zip"
-
-            echo "Downloading artifact from $DOWNLOAD_URL..."
+            echo "Downloading 'screenshots' artifact from $DOWNLOAD_URL..."
             if ! curl -L -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" -o "$DOWNLOAD_PATH" "$DOWNLOAD_URL"; then
-                echo "Error downloading artifact."
+                echo "Error downloading 'screenshots' artifact."
                 exit 1
             fi
-            echo "Artifact downloaded to $DOWNLOAD_PATH"
+            echo "'screenshots' artifact downloaded to $DOWNLOAD_PATH"
             unzip_artifact "$DOWNLOAD_PATH" "$UNZIP_FOLDER"
         else
-            echo "No artifact download URL found."
+            echo "No 'screenshots' artifact download URL found ⛔"
         fi
     else
-        echo "No artifacts found."
+        echo "No artifacts found ⛔"
     fi
 else
-    echo "The latest workflow run has not completed successfully."
+    echo "The latest workflow run has not completed successfully ❌"
     exit 1
 fi
