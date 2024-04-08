0edfa6a49b469e0d77e141d3df5f63815d95b78e
Karl Cardenas <karl@spectrocloud.com>
2024-04-08 08:44:52 -0700

    chore: updates to components and dependencies

--- src/theme/Admonition/Icon/TechPreview.js
+++ src/theme/Admonition/Icon/TechPreview.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFlag } from "@fortawesome/free-solid-svg-icons";
+// import { faFlag } from "@fortawesome/free-solid-svg-icons";
 
 export default function IconTechPreview() {
-  return <FontAwesomeIcon icon={faFlag} />;
+  return <FontAwesomeIcon />;
 }
