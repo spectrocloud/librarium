37c00e8ded2b84ab36b924a67399b4c062eaae37
Lenny Chen <lenny.chen@spectrocloud.com>
2024-05-02 17:46:06 -0700

    remove redundant redirect

--- redirects.js
+++ redirects.js
@@ -393,10 +393,6 @@ const redirects = [
     to: "/clusters/edge/site-deployment/deploy-custom-registries/deploy-private-registry/",
   },
   {
-    from: "/clusters/edge/networking/local-registry",
-    to: "/clusters/edge/site-deployment/deploy-custom-registries/local-registry",
-  },
-  {
     from: "/clusters/edge/site-deployment/deploy-private-registry",
     to: "/clusters/edge/site-deployment/deploy-custom-registries/deploy-private-registry",
   },
