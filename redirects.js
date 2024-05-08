efd606cd9ebf079cb777bfaa877f6fdec144b11a
Lenny Chen <lenny.chen@spectrocloud.com>
2024-05-08 11:15:21 -0700

    fix redirects

--- redirects.js
+++ redirects.js
@@ -393,10 +393,6 @@ const redirects = [
     to: "/clusters/edge/site-deployment/deploy-custom-registries/deploy-private-registry/",
   },
   {
-    from: "/clusters/edge/site-deployment/deploy-private-registry",
-    to: "/clusters/edge/site-deployment/deploy-custom-registries/deploy-private-registry",
-  },
-  {
     from: [
       "/security/security-bulletins/",
       "/security/security-bulletins/cve-reports/",
