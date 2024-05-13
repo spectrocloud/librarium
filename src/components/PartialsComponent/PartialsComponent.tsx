8abbaf5d4ac49f2d0bbb915ac53062c6992767d8
addetz <43963729+addetz@users.noreply.github.com>
2024-05-13 20:40:15 +0100

    docs: add comments to component

--- src/components/PartialsComponent/PartialsComponent.tsx
+++ src/components/PartialsComponent/PartialsComponent.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// Import each partial here 
 import PaletteSetup from "./_partials/getting-started/_palette-setup.mdx"
 
 interface InputProperty {
@@ -8,13 +9,15 @@ interface InputProperty {
 
 interface PartialsComponentDetails {
     name: string;
+    // Pass the key-value property pairs 
     props: InputProperty[];
 }
 
 interface PartialsMap {
     [key: string]: React.ReactElement;
 }
-  
+ 
+// Maintain a map of existing partials 
 export const partials: PartialsMap = {
     PaletteSetup: <PaletteSetup />,
 }; 
@@ -26,6 +29,7 @@ export default function PartialsComponent(details : PartialsComponentDetails) : 
             concat(". Check partial names in PartialsComponent."))
     }
 
+    // Map elements to index signatures
     var propAttribute: {[key: string] : string} = {}
     details.props.map((val) => {
         propAttribute[val.key] = val.value
