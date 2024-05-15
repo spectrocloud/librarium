a30ff5cb5d1a6721d7e0c481456553150a630b00
Karl Cardenas <karl@spectrocloud.com>
2024-05-15 15:14:10 -0700

    chore: more redirect fixes

--- redirects.js
+++ redirects.js
@@ -432,46 +432,6 @@ const redirects = [
     from: "/palette-cli/commands/",
     to: "/automation/palette-cli/commands/",
   },
-
-  {
-    from: "/palette-cli/commands/docs/",
-    to: "/automation/palette-cli/commands/docs/",
-  },
-
-  {
-    from: "/palette-cli/commands/ec/",
-    to: "/automation/palette-cli/commands/ec/",
-  },
-
-  {
-    from: "/palette-cli/commands/fips-validate/",
-    to: "/automation/palette-cli/commands/fips-validate/",
-  },
-
-  {
-    from: "/palette-cli/commands/login/",
-    to: "/automation/palette-cli/commands/login/",
-  },
-
-  {
-    from: "/palette-cli/commands/pcg/",
-    to: "/automation/palette-cli/commands/pcg/",
-  },
-
-  {
-    from: "/palette-cli/commands/pde/",
-    to: "/automation/palette-cli/commands/pde/",
-  },
-
-  {
-    from: "/palette-cli/commands/project/",
-    to: "/automation/palette-cli/commands/project/",
-  },
-
-  {
-    from: "/palette-cli/commands/validator/",
-    to: "/automation/palette-cli/commands/validator/",
-  },
   {
     from: "/user-management/palette-resource-limits/",
     to: "/tenant-settings/palette-resource-limits/",
