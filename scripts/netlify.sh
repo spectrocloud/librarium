f597020d2d524939f705c1c06166305b682d9dd7
Karl Cardenas <karl@spectrocloud.com>
2024-05-21 20:17:19 -0700

    chore: updated netlify.sh

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# List of branches to NOT create a Netlify preview
-# Master branch does not need a preview
-# Release branches get a preview through docs-latest.spectrocloud.com
+############################################
+# This script checks if a Netlify context is for branch-deploy. 
+# Netlify branch-deploy contexts are only allowed for branches that match version-*. This script is used in the Netlify build settings to determine if a preview should be created.
+# This script is created to prevent both a build-preview and a branch-deploy preview from being created for the same branch at the same time.
+############################################
 
-
-# List of branches to NOT create an automatic Netlify preview. This also includes branch-deploy previews.
-disallowed_branches=("master" "release-*")
+target_branch=$1
+context=$CONTEXT
 
 # Get current branch name
 current_branch=$(git branch --show-current)
@@ -14,25 +15,26 @@ current_branch=$(git branch --show-current)
 # Use HEAD if current_branch is empty
 [ -z "$current_branch" ] && current_branch="$HEAD"
 
-echo "Branch name: $current_branch"
+echo "Current branch name: $current_branch"
+echo "Context: $context"
 
-# Initialize not_allowed flag
-not_allowed=0
+# Initialize allowed flag
+allowed=1
 
-# Compare current_branch against disallowed list
-for disallowed in "${disallowed_branches[@]}"
-do
-  if [[ "$current_branch" == $disallowed ]]; then
-    not_allowed=1
-    break
+# Check if context is branch-deploy and current branch matches version-*
+if [[ "$context" == "branch-deploy" ]]; then
+  if [[ "$current_branch" == version-* ]]; then
+    allowed=1
+  else
+    allowed=0
   fi
-done
+fi
 
-# Exit based on not_allowed flag
-if [ $not_allowed -eq 1 ]; then
-  echo "Not allowed to create a Netlify preview"
-  exit 0
-else
+# Exit based on allowed flag
+if [ $allowed -eq 1 ]; then
   echo "Allowed to create a Netlify preview"
   exit 1
+else
+  echo "Not allowed to create a Netlify preview"
+  exit 0
 fi
