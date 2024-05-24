7ce41b45a5a1bc08f4bf247ebc3731a19ef2ad3a
yuliiiah <horbenko.yuliya@gmail.com>
2024-05-24 13:58:30 +0200

    chore: Fix formatting

--- _partials/index.ts
+++ _partials/index.ts
@@ -1,2 +1,2 @@
 // This file is generated. DO NOT EDIT!
-export * as partialexample2689 from '@site/_partials/_partial_example.mdx';
+export * as partialexample2689 from "@site/_partials/_partial_example.mdx";
