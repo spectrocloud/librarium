d4406c203f8018c0681782d8c8219d28239b689a
Adelina Simion <43963729+addetz@users.noreply.github.com>
2024-06-24 16:16:12 +0200

    docs: create cloud-centric Getting Started DOC-1068 (#2389)
    * docs: add navigation card components
    
    * chore: Add a few more grid styles
    
    * docs: add Jest test and adjust Getting Started landing page
    
    * docs: fix formatting
    
    * docs: fix lint errors in SimpleCard
    
    * docs: refactor grid test DOC-1067
    
    * docs: reduce duplication in SimpleCard test
    
    * docs: reduce duplication in SimpleCard test
    
    * docs: fix formatting DOC-1067
    
    * docs: update SimpleCardGrid naming DOC-1067
    
    * docs: create cloud-centric Getting Started prototype DOC-1068
    
    * docs: fix formatting
    
    * docs: adjust nav cards and rebase
    
    * docs: update prototype DOC-1068
    
    * docs: move dashboard to introduction DOC-1068
    
    * docs: update AWS section DOC-1068
    
    * docs: update Azure section DOC-1068
    
    * docs: update GCP section DOC-1068
    
    * docs: add tags to getting started DOC-1068
    
    * docs: update formatting DOC-1068
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * docs: update GCP landing page DOC-1068
    
    * Apply suggestions from code review
    
    Co-authored-by: Karl Cardenas <karl@spectrocloud.com>
    
    * docs: adjust setup fro getting started DOC-1068
    
    * docs: remove video from deploy cluster DOC-1068
    
    * docs: update imports in AWS DOC-1068
    
    * docs: fix formatting DOC-1068
    
    * docs: fix broken links DOC-1068
    
    * docs: fix broken links DOC-1068
    
    * docs: adjust partials usage DOC-1068
    
    * docs: use partials in getting started DOC-1068
    
    * docs: use hello-universe pack in cluster profile DOC-1068
    
    * Optimised images with calibre/image-actions
    
    * docs: fix registries link DOC-1068
    
    * docs: update aws cluster profile updates page DOC-1068
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * docs: update azure tutorial with hello-uni pack DOC-1068
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * docs: update gcp tutorial DOC-1068
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * Optimised images with calibre/image-actions
    
    * docs: update azure guide with correct assets DOC-1068
    
    ---------
    
    Co-authored-by: yuliiiah <horbenko.yuliya@gmail.com>
    Co-authored-by: vault-token-factory-spectrocloud[bot] <133815545+vault-token-factory-spectrocloud[bot]@users.noreply.github.com>
    Co-authored-by: Karl Cardenas <karl@spectrocloud.com>

--- redirects.js
+++ redirects.js
@@ -80,10 +80,6 @@ const redirects = [
     to: `/introduction/dashboard`,
   },
   {
-    from: `/getting-started/cluster-profiles`,
-    to: `/getting-started/introduction`,
-  },
-  {
     from: `/clusters/public-cloud/eks`,
     to: `/clusters/public-cloud/aws/eks`,
   },
