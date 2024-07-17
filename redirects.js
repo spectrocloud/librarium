03c8798ddd7a2b42942e961628bae6853c2de470
Karl Cardenas <29551334+karl-cardenas-coding@users.noreply.github.com>
2024-07-17 14:37:10 -0700

    Security Bulletin Changes (#3120) (#3408)
    * docs: DOC-1241
    
    * docs: draft
    
    * chore: updated with link
    
    * docs: updated disclosures
    
    * docs: updated
    
    * chore: updated
    
    * docs: updated
    
    * docs: updates
    
    * chore: updates
    
    * chore: fix
    
    * chore: missing URLs
    
    * chore: updated prettier to exclude cve-page
    
    * chore: added N/A versus leaving blank
    
    * docs: updated CVEs
    
    * docs: update
    
    * docs: added airgap
    
    * docs: fixed minor issue
    
    * docs: fix broken URL
    
    * docs: updated intro langugae

--- redirects.js
+++ redirects.js
@@ -466,7 +466,6 @@ const redirects = [
     from: "/user-management/palette-resource-limits/",
     to: "/tenant-settings/palette-resource-limits/",
   },
-
   // Redirects for tutorials that were removed from docs
   {
     from: "/registries-and-packs/deploy-pack",
@@ -487,6 +486,14 @@ const redirects = [
   {
     from: "/clusters/edge/site-deployment/deploy-cluster",
     to: "/tutorials/edge/deploy-cluster",
+  },
+  {
+    from: "/security-bulletins/index/",
+    to: "/unlisted/index/",
+  },
+  {
+    from: "/security-bulletins/cve-reports/",
+    to: "/security-bulletins/reports/",
   },
 ];
 
