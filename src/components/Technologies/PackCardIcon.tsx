183ecb0ee17a6e5c443a0aceac90e8d3a42985fe
Karl Cardenas <karl@spectrocloud.com>
2024-08-20 13:17:15 -0700

    chore: fix import with idealImage

--- src/components/Technologies/PackCardIcon.tsx
+++ src/components/Technologies/PackCardIcon.tsx
@@ -3,7 +3,6 @@ import React, { useState, useEffect, ReactElement } from "react";
 import styles from "./PackCardIcon.module.scss";
 import IconMapper from "@site/src/components/IconMapper/IconMapper";
 import Image from "@theme/IdealImage";
-
 interface PackCardIconProps {
   appType?: string;
   logoUrl?: string;
@@ -13,28 +12,20 @@ interface PackCardIconProps {
 
 export default function PackCardIcon({ appType, logoUrl, type, className }: PackCardIconProps) {
   const [icon, setIcon] = useState<ReactElement | null>(null);
-
   useEffect(() => {
-    const loadImage = async () => {
-      if (logoUrl) {
-        try {
-          if (appType === "app") {
-            setIcon(<img src={logoUrl} />);
-          } else {
-            const img = (await import(`/static/img/packs/${logoUrl}`)).default;
-            setIcon(<Image img={img} />);
-          }
-        } catch (e) {
-          console.error("Error loading pack icon image", e);
-          type = type ? setIcon(<IconMapper type={type} />) : setIcon(null);
+    if (logoUrl) {
+      try {
+        if (appType === "app") {
+          setIcon(<img src={logoUrl} />);
+        } else {
+          setIcon(<Image img={require(`/static/img/packs/${logoUrl}`)} />);
         }
-      } else {
-        type = type ? setIcon(<IconMapper type={type} />) : setIcon(null);
+      } catch (e) {
+        type ? setIcon(<IconMapper type={type} />) : setIcon(null);
       }
-    };
-
-    loadImage();
+    } else {
+      type ? setIcon(<IconMapper type={type} />) : setIcon(null);
+    }
   }, [logoUrl]);
 
   return <div className={`${className} ${styles.imageWrapper}`}>{icon}</div>;
-}
