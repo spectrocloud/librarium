6f05449f03ad7eaf7add27d7d0505a864f313b0f
Karl Cardenas <karl@spectrocloud.com>
2024-09-16 16:15:13 -0700

    chore: fixed route duplicates

--- plugins/security-bulletins.js
+++ plugins/security-bulletins.js
@@ -74,13 +74,17 @@ async function pluginSecurityBulletins(context, options) {
       // We need to add a route for each CVE. Let's combine all entries into a single array.
       const allCVEs = Object.values(GlobalCVEData).reduce((acc, curr) => acc.concat(curr), []);
 
+      // remove any duplicate CVEs
+      const uniqueCVEs = allCVEs.filter((item, index, self) => index === self.findIndex((t) => t.cve === item.cve));
+
       // In here we loop through all the CVEs and create a route for each one.
       // The data for each route is passed as a prop to the component.
-      for (const item of allCVEs) {
+      for (const item of uniqueCVEs) {
         const jsonPath = await createData(`${item.cve}.json`, JSON.stringify(item, null, 2));
 
+        const path = `/security-bulletins/reports/${item.cve}`;
         addRoute({
-          path: `/security-bulletins/reports/${item.cve.toLowerCase()}`,
+          path: `/security-bulletins/reports/${path.toLowerCase()}`,
           component: "@site/src/components/CVECard",
           metadata: {
             sourceFilePath: "../docs/docs-content/security-bulletins/reports/reports.mdx",
@@ -89,6 +93,7 @@ async function pluginSecurityBulletins(context, options) {
             cve: jsonPath,
           },
         });
+        console.log(`Created route for ${path.toLowerCase()}`);
       }
     },
   };
--- src/components/CVECard/CVECard.tsx
+++ src/components/CVECard/CVECard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function CveReportsTable(props) {
+export default function CVECard(props) {
   const { cve } = props; // Accessing cve from props
 
   if (!cve) {
