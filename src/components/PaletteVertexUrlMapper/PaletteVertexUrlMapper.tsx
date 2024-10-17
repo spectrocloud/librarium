5db615ebb4fbc4e14ef59fcd1936b2c4c66673a1
Carolina Delwing Rosa <carolina.delwing@spectrocloud.com>
2024-10-17 11:52:34 -0400

    docs: update url mapper component

--- src/components/PaletteVertexUrlMapper/PaletteVertexUrlMapper.tsx
+++ src/components/PaletteVertexUrlMapper/PaletteVertexUrlMapper.tsx
@@ -3,15 +3,19 @@ import VersionedLink from "../VersionedLink/VersionedLink";
 
 // This component is used to generate the correct URL for the palette and vertex versions of the documentation.
 // It takes the edition, text, and URL as props and returns the correct URL based on the edition.
+// If the vertex and palette pages have different URLs, the component takes palettePath and vertexPath as individual props and returns the correct URL.
 
 interface ComponentProperties {
   [key: string]: string;
 }
 
 export default function PaletteVertexUrlMapper(props: ComponentProperties) {
-  const { edition, text, url } = props;
+  const { edition, text, url, palettePath, vertexPath } = props;
   const isPalette = edition?.toLowerCase() === "palette";
-  const mappedUrl = isPalette ? `/enterprise-version${url}` : `/vertex${url}`;
+  const baseUrl = isPalette ? "/enterprise-version" : "/vertex";
+
+  const mappedUrl =
+    palettePath && vertexPath ? `${baseUrl}${isPalette ? palettePath : vertexPath}` : `${baseUrl}${url}`;
 
   return <VersionedLink url={mappedUrl} text={text} />;
 }
