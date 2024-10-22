f3353d8108032a84b69d1dfdc4c67be91ad883d3
addetz <43963729+addetz@users.noreply.github.com>
2024-10-22 12:48:47 +0100

    docs: add clean up images job  DOC-1232

---
+++ scripts/find-unused-images.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Enable error handling
+set -e
+
+echo "Checking for unused image files in static/assets/docs/images folder..."
+
+# Create a list of all the images we have and save it to a json.
+# Trim the path static/assets/docs/images.
+find static/assets/docs/images -type f ! -name ".DS_STORE" ! -name ".DS_Store" | sed 's|static/assets/docs/images||g'  > all_images.json
+
+# List all the version branches
+branches="master"
+version_branches=$(git branch --list "version-[0-9]-[0-9]")
+branches+=" $version_branches"
+
+for current_branch in $branches; do
+    git checkout $current_branch
+    echo "Current branch: $current_branch"
+    git pull
+
+    find docs -type f -name "*.md" -exec grep -Hn "\.webp" {} + > docs_used_images.json
+    find _partials -type f -name "*.mdx" -exec grep -Hn "\.webp" {} + > partials_used_images.json
+    cat docs_used_images.json partials_used_images.json > used_images.json
+
+    line_number=1
+    for img in $(cat all_images.json); do
+        if grep -q $img used_images.json; then
+            sed -i .bak "${line_number}s|.*|${img},FOUND_USED|" all_images.json
+        fi
+        ((line_number++))
+    done
+    
+done
+
+# Remove all marked used files to make up the list 
+sed '/FOUND_USED/d' all_images.json > unused_images.json
+
+# Clean up files
+rm all_images.json
+rm all_images.json.bak
+rm docs_used_images.json
+rm partials_used_images.json
+rm used_images.json
