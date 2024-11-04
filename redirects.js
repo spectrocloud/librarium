aae1de381c231c9504f5f3647eca6bd8ab2b2e99
Karl Cardenas <29551334+karl-cardenas-coding@users.noreply.github.com>
2024-11-04 13:55:27 -0700

    docs: PEM-6244 Workaround (#4568)
    * chore: create emergency escape hatch
    
    * chore: fix eslint errors
    
    * chore: fix env condition
    
    * ci: added packs env variable
    
    * chore: update package.json

--- redirects.js
+++ redirects.js
@@ -1,4 +1,29 @@
-const redirects = [
+const fetchAndFilterUrls = require("./utils/xml/packExtracter");
+
+/*
+The logigc below is all related to the event that the environment variable DISABLE_PACKS_INTEGRATIONS is set to true.
+The function fetchAndFilterUrls is used to fetch the sitemap.xml file from production. 
+The assumption is that the sitemap.xml file is up to date and contains all the URLs for the packs integrations.
+We then filter the URLs that start with the prefix "https://docs.spectrocloud.com/integrations/packs/" and store them in the packRedirects array.
+The redirects array is then updated with the packRedirects array.
+This allows to prevent broken links when the packs integrations are disabled.
+*/
+
+let packRedirects = [];
+
+if (process.env.DISABLE_PACKS_INTEGRATIONS === "true") {
+  const sitemapUrl = "https://docs.spectrocloud.com/sitemap.xml";
+  const urlPrefix = "https://docs.spectrocloud.com/integrations/packs/";
+
+  fetchAndFilterUrls(sitemapUrl, urlPrefix).then((paths) => {
+    packRedirects = paths.map((path) => ({
+      from: path,
+      to: "/integrations/",
+    }));
+  });
+}
+
+let redirects = [
   {
     from: `/api/`,
     to: `/api/introduction/`,
@@ -448,5 +473,9 @@ const redirects = [
     to: "/integrations/",
   },
 ];
+
+if (packRedirects.length > 0) {
+  redirects = redirects.concat(packRedirects);
+}
 
 module.exports = redirects;
