0d11f953cc7ac78f8a30b7da51c2f3dad5ba64ae
Karl Cardenas <29551334+karl-cardenas-coding@users.noreply.github.com>
2024-11-04 14:46:14 -0700

    docs: PEM-6244 Workaround (#4568) (#4590)
    * chore: create emergency escape hatch
    
    * chore: fix eslint errors
    
    * chore: fix env condition
    
    * ci: added packs env variable
    
    * chore: update package.json

--- src/components/Integrations/Packs/Packs.tsx
+++ src/components/Integrations/Packs/Packs.tsx
@@ -2,8 +2,24 @@ import React from "react";
 import Integrations from "@site/src/components/Technologies";
 import { usePluginData } from "@docusaurus/useGlobalData";
 import PacksIntegrationsPluginData from "../IntegrationTypes";
+import Admonition from "@theme/Admonition";
 
 export default function Packs() {
-  const { packs, repositories } = usePluginData("plugin-packs-integrations") as PacksIntegrationsPluginData;
-  return <Integrations data={packs} repositories={repositories} />;
+  try {
+    const { packs, repositories } = usePluginData("plugin-packs-integrations") as PacksIntegrationsPluginData;
+    if (!packs && !repositories) {
+      throw new Error("No packs data is available at the moment. Returning admonition with warning message.");
+    }
+
+    return <Integrations data={packs} repositories={repositories} />;
+  } catch (_e) {
+    return (
+      <Admonition type="warning">
+        <p>
+          No packs data is available at the moment. This may be due to maintenance or an issue with the data source. Try
+          again later.
+        </p>
+      </Admonition>
+    );
+  }
 }
