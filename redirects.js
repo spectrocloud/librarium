956454ecd4bc0ddeefa974236173c272d3692f65
Karl Cardenas <29551334+karl-cardenas-coding@users.noreply.github.com>
2024-11-15 07:53:14 -0700

    chore: add ngrok (#4720)
    (cherry picked from commit 345ad4bf4923cdf24e6bfc87e9adfc2bc9264d52)

--- redirects.js
+++ redirects.js
@@ -469,6 +469,7 @@ let redirects = [
       "/integrations/trident",
       "/integrations/ubuntu",
       "/integrations/vsphere-csi",
+      "/integrations/ngrok",
     ],
     to: "/integrations/",
   },
