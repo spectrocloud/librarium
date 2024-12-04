9bc0ca465c6e8e10d2f8342f55670670065edce4
Nageswari Raghunathan <88926279+nage1234@users.noreply.github.com>
2024-12-04 23:41:57 +0530

    chore: PEM-6153 fixed the fragment url to follow the actual content (#4697)
    * PEM-6153-fixed the fragment url to follow the actual content
    
    * ci: auto-formatting prettier issues
    
    * changed the click as scrollIntoView
    
    * reverting back to click
    
    * fixed the fragment url for main Readme as well
    
    * ci: auto-formatting prettier issues
    
    * cleaned up
    
    * ci: auto-formatting prettier issues
    
    * addressed the review comments
    
    * added fragment link support for h1 as well
    
    ---------
    
    Co-authored-by: nage1234 <nage1234@users.noreply.github.com>
    Co-authored-by: Karl Cardenas <29551334+karl-cardenas-coding@users.noreply.github.com>
    (cherry picked from commit db85b2d12942e884706ed63504fe95635e7e188b)

--- src/components/PacksInformation/PacksInformation.tsx
+++ src/components/PacksInformation/PacksInformation.tsx
@@ -13,6 +13,7 @@ interface PacksData {
     name: string;
     version: string;
     parent: string;
+    tab: string;
   };
 }
 
@@ -30,7 +31,9 @@ export default function Packs(props: Packs) {
         const data = props?.route?.data;
         return data ? (
           <Switch>
-            <Redirect to={`/integrations/packs?pack=${data.name}&version=${data.version}&parent=${data.parent}`} />
+            <Redirect
+              to={`/integrations/packs?pack=${data.name}&version=${data.version}&parent=${data.parent}&tab=${data.tab}`}
+            />
           </Switch>
         ) : (
           <PacksReadme />
