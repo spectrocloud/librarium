10272dd64bced06fdc99e66f2b071d467182339c
addetz <43963729+addetz@users.noreply.github.com>
2024-12-10 16:29:02 +0000

    docs: change simple cards to use versioned link DOC-1502

--- src/components/VersionedLink/VersionedLink.tsx
+++ src/components/VersionedLink/VersionedLink.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import GetVersionPath from "./CheckVersion";
 
 interface ComponentProperties {
-  [key: string]: string;
+  text?: string;
+  url: string;
+  component?: React.ReactElement;
 }
 
 export default function VersionedLink(props: ComponentProperties) {
@@ -17,9 +19,23 @@ export default function VersionedLink(props: ComponentProperties) {
       "Versioned links should not be used for external URLs. Please use the default markdown syntax instead."
     );
   }
+
+  // Versioning detected
   if (path != "") {
     const versionedURL = path.concat(props.url);
+
+    // Component Mode detected
+    if (props.component != null) {
+      return <a href={versionedURL}>{props.component}</a>;
+    }
+
+    // Text mode detected
     return <a href={versionedURL}>{props.text}</a>;
+  }
+
+  // Component mode of versioned link detected without versioning
+  if (props.component != null) {
+    return <a href={props.url}>{props.component}</a>;
   }
 
   return <a href={props.url}>{props.text}</a>;
