ad4103ce7f0fa02c242c1c35df3350027607fe33
Karl Cardenas <29551334+karl-cardenas-coding@users.noreply.github.com>
2024-12-13 12:32:30 -0700

    docs: DOC-1520 Security Bulletin Fix (#5055)
    * chore: update object
    
    * chore: DOC-1520 fixed affected products content logic
    
    * chore: update limit
    
    * chore: update table to easier scan
    
    (cherry picked from commit ace8016b286d99dcf12e6ba6b12c8a1a01928e81)

--- utils/helpers/affected-table.test.js
+++ utils/helpers/affected-table.test.js
@@ -3,15 +3,14 @@ const { generateMarkdownTable } = require("./affected-table");
 describe("generateMarkdownTable", () => {
   it("should generate a markdown table for two products with mixed impact", () => {
     const cveImpactMap = {
-      versions: ["4.4.20", "4.5.3"],
-      impactsPaletteEnterprise: true,
-      impactsPaletteEnterpriseAirgap: false,
-      impactsVerteX: false,
-      impactsVerteXAirgap: false,
+      palette: { impacts: true, versions: ["4.4.20", "4.5.3"] },
+      paletteAirgap: { impacts: false, versions: [] },
+      vertex: { impacts: false, versions: [] },
+      vertexAirgap: { impacts: false, versions: [] },
     };
 
     const expectedTable = `| Version | Palette Enterprise | Palette Enterprise Airgap | VerteX | VerteX Airgap |
-|-|--------|--------|--------|--------|
+| - | -------- | -------- | -------- | -------- |
 | 4.5.3 | Impacted | No Impact | No Impact | No Impact |
 | 4.4.20 | Impacted | No Impact | No Impact | No Impact |`;
 
@@ -20,24 +19,22 @@ describe("generateMarkdownTable", () => {
 
   it("should return investigation message when all products are not impacted", () => {
     const cveImpactMap = {
-      versions: ["4.4.20", "4.5.3"],
-      impactsPaletteEnterprise: false,
-      impactsPaletteEnterpriseAirgap: false,
-      impactsVerteX: false,
-      impactsVerteXAirgap: false,
+      palette: { impacts: false, versions: [] },
+      paletteAirgap: { impacts: false, versions: [] },
+      vertex: { impacts: false, versions: [] },
+      vertexAirgap: { impacts: false, versions: [] },
     };
 
-    const expectedMessage = "Investigation is ongoing to determine how this vulnerability affects our products";
+    const expectedMessage = "Investigation is ongoing to determine how this vulnerability affects our products.";
     expect(generateMarkdownTable(cveImpactMap)).toBe(expectedMessage);
   });
 
   it("should throw an error when products are impacted but no versions are provided", () => {
     const cveImpactMap = {
-      versions: [],
-      impactsPaletteEnterprise: true,
-      impactsPaletteEnterpriseAirgap: false,
-      impactsVerteX: false,
-      impactsVerteXAirgap: false,
+      palette: { impacts: true, versions: [] },
+      paletteAirgap: { impacts: false, versions: [] },
+      vertex: { impacts: false, versions: [] },
+      vertexAirgap: { impacts: false, versions: [] },
     };
 
     expect(() => generateMarkdownTable(cveImpactMap)).toThrow(
