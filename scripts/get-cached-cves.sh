acc12fea5bcaf82e70ae6a401a11349854615a4f
Lenny Chen <55669665+lennessyy@users.noreply.github.com>
2025-01-07 14:49:01 -0800

    chore: add caching for CVE data (#5281)
    * docs: add caching for CVE data
    
    * docs: modify folders
    
    * docs: clean up comments
    
    * chore: add additional try catch
    
    * docs: test with error
    
    * docs: remove error for new test
    
    * docs: modify unpack and add error back
    
    * ci: auto-formatting prettier issues
    
    * docs: add success status filter
    
    * chore: add status filter
    
    * docs: fix command error
    
    * docs: remove test error
    
    * docs: remove test error
    
    * docs: add filter to packs
    
    * docs: modify one more error code
    
    * docs: add to readme
    
    * test with dummy dso value
    
    * docs: change dso token back
    
    ---------
    
    Co-authored-by: lennessyy <lennessyy@users.noreply.github.com>
    (cherry picked from commit 472d92588f67a14ed245c63c6006d29419c4fac8)

--- scripts/get-cached-cves.sh
+++ scripts/get-cached-cves.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+
 # Enable error handling
 set -e
 
-echo "Starting fetch of cached packs..."
+echo "Starting fetch of cached cves..."
 
 if command -v gh &> /dev/null
 then
@@ -22,28 +23,25 @@ else
     echo "ℹ️ Please log in with 'gh auth login'."
     exit 1
 fi
+ # Find the latest cves upload workflow.
 
- # Find the latest packs upload workflow.
 run_id=$(gh run list --workflow="post_release.yaml" --status=success --limit 1 --json databaseId | jq -r '.[0].databaseId')
 
 # Remove any downloaded artifacts, should they exist.
 rm -rf ./downloaded_artifacts
-        
-# Download the latest artifact to a new dir.
-gh run download ${run_id} --name build-packs --dir ./downloaded_artifacts
 
-echo "✅ Cached packs artifact downloaded."
+# Download the latest artifact to a new dir.
+gh run download ${run_id} --name security-bulletins --dir ./downloaded_artifacts
+echo "✅ Cached CVEs artifact downloaded."
 
 # Ensure the correct folders exist.
-mkdir -p .docusaurus/packs-integrations
+mkdir -p .docusaurus/security-bulletins/default
 
 # Move the files to their correct places in the checked out repository
-mv downloaded_artifacts/.docusaurus/packs-integrations/* .docusaurus/packs-integrations 
-mkdir -p static/img/packs
-mv downloaded_artifacts/build/packs/* static/img/packs
+mv downloaded_artifacts/data.json .docusaurus/security-bulletins/default/data.json
 
 # Clean up. 
 rm -rf downloaded_artifacts
 
-echo "✅ Completed fetch of cached packs." 
+echo "✅ Completed fetch of cached CVEs." 
 echo "⏭️ You can now execute 'make start' or 'make build'."
