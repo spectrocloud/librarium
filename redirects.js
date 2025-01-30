f6707067f0d51e936414fc0ec286e0167c0933fe
Carolina Delwing Rosa <carolina.delwing@spectrocloud.com>
2025-01-30 13:39:18 -0300

    docs: remove extra redirect rule from 4-4

--- redirects.js
+++ redirects.js
@@ -591,10 +591,6 @@ let redirects = [
     to: "/legal-licenses/oss-licenses-index/",
   },
   {
-    from: "/clusters/cluster-management/noc-ui/",
-    to: "/clusters/cluster-management/cluster-map-filters/",
-  },
-  {
     from: "/clusters/cluster-management/reconfigure/",
     to: "/clusters/cluster-management/node-pool/",
   },
