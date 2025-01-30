247d9417b1d74a69284326f49ff5e63142b0e0b4
caroldelwing <carolina.delwing@spectrocloud.com>
2025-01-30 10:17:34 -0300

    docs: add delete node pool section (#5569)
    * docs: add delete node pool section
    
    * Apply suggestions from code review
    
    Co-authored-by: Adelina Simion <43963729+addetz@users.noreply.github.com>
    
    * docs: remove reconfigure page
    
    * docs: improve deletion warning
    
    ---------
    
    Co-authored-by: Adelina Simion <43963729+addetz@users.noreply.github.com>

--- redirects.js
+++ redirects.js
@@ -483,6 +483,10 @@ let redirects = [
     to: "/security-bulletins/reports/",
   },
   {
+    from: "/clusters/cluster-management/reconfigure/",
+    to: "/clusters/cluster-management/node-pool/",
+  },
+  {
     from: [
       "/integrations/antrea-cni",
       "/integrations/aws-autoscaler",
