0ffedfa9675fcd0abefe8b28a306d3dc3ea81f5a
Lenny Chen <55669665+lennessyy@users.noreply.github.com>
2025-02-28 13:15:53 -0800

    docs: Backport/version 4 0/pr 5876 (#5912) (#5913)
    * chore: add kapa branch (#4858)
    
    * feat: add kapa branch
    
    * docs: add kapa widget
    
    * docs: update logo
    
    * docs: add consent screen
    
    * docs: static script
    
    * docs: fix script issue
    
    * docs: change back to online loading for test
    
    * docs: cors anonymous
    
    * docs: revert back to lonline loading
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    * docs: change source script location
    
    * ci: auto-formatting prettier issues
    
    * docs: change logos and theme colors DOC-1587
    
    * docs: further styling
    
    * docs: style packs component
    
    * docs: style cve table
    
    * docs: style getting started cards
    
    * docs: change font
    
    * docs: adjust size of logo and points of interest
    
    * ci: auto-formatting prettier issues
    
    * Optimised images with calibre/image-actions
    
    * docs: style tooltips
    
    * docs: change color of privacy link
    
    * docs: update background color
    
    * docs: update styles
    
    * docs: remove tags from underlines
    
    * docs: fix navbar color and add 10% opacity to admonitions
    
    * docs: style as support chat
    
    * docs: style kapa
    
    * docs: adjust color
    
    * docs: edit eslint ignore
    
    * docs: fix eslint
    
    * docs: add disclaimer
    
    * docs: remove old logo
    
    * docs: add command to bring up chat
    
    * docs: add browser shortcut
    
    * docs: comment out shortcut script
    
    * docs: undo temporary netlify change
    
    * docs: add release notes snippets
    
    * docs: punctuation
    
    * docs: remove netlify changes
    
    * docs: keep favicon the same
    
    * Apply suggestions from code review
    
    
    
    ---------
    
    
    
    
    * chore: add quick keyboard shortcut to open kapa (#5876)
    
    * feat: add kapa branch
    
    * docs: add kapa widget
    
    * docs: update logo
    
    * docs: add consent screen
    
    * docs: static script
    
    * docs: fix script issue
    
    * docs: change back to online loading for test
    
    * docs: cors anonymous
    
    * docs: revert back to lonline loading
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    * Update docusaurus.config.js
    
    * docs: change source script location
    
    * ci: auto-formatting prettier issues
    
    * docs: change logos and theme colors DOC-1587
    
    * docs: further styling
    
    * docs: style packs component
    
    * docs: style cve table
    
    * docs: style getting started cards
    
    * docs: change font
    
    * docs: adjust size of logo and points of interest
    
    * ci: auto-formatting prettier issues
    
    * Optimised images with calibre/image-actions
    
    * docs: style tooltips
    
    * docs: change color of privacy link
    
    * docs: update background color
    
    * docs: update styles
    
    * docs: remove tags from underlines
    
    * docs: fix navbar color and add 10% opacity to admonitions
    
    * docs: style as support chat
    
    * docs: style kapa
    
    * docs: adjust color
    
    * docs: edit eslint ignore
    
    * docs: fix eslint
    
    * docs: add disclaimer
    
    * docs: remove old logo
    
    * docs: add command to bring up chat
    
    * docs: add browser shortcut
    
    * docs: comment out shortcut script
    
    * docs: undo temporary netlify change
    
    * docs: add release notes snippets
    
    * docs: punctuation
    
    * docs: remove netlify changes
    
    * docs: keep favicon the same
    
    * chore: add shortcut to open
    
    * docs: modify dialog location
    
    * docs: add keyboard shortcut
    
    ---------
    
    
    
    
    ---------
    
    Co-authored-by: addetz <43963729+addetz@users.noreply.github.com>
    Co-authored-by: vault-token-factory-spectrocloud[bot] <133815545+vault-token-factory-spectrocloud[bot]@users.noreply.github.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -247,7 +247,7 @@ const config = {
     },
     {
       src: "/scripts/kapa-shortcut.js",
-      async: true,
+      async: false,
     },
     {
       src: "/scripts/kapa-widget.bundle.js",
@@ -273,6 +273,7 @@ const config = {
       "data-conversation-button-icons-only": "true",
       "data-modal-size": "80%",
       "data-modal-lock-scroll": "false",
+      "data-modal-inner-position-left": "auto",
       async: true,
     },
     {
