03cbc0d3fe9a95bca1d4ee091e2caf421fcb1e6e
caroldelwing <carolina.delwing@spectrocloud.com>
2025-03-04 13:27:18 -0500

    docs: automate permissions update DOC 1613 (#5715)
    * chore: bump semver from 7.6.3 to 7.7.1
    
    Bumps [semver](https://github.com/npm/node-semver) from 7.6.3 to 7.7.1.
    - [Release notes](https://github.com/npm/node-semver/releases)
    - [Changelog](https://github.com/npm/node-semver/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/npm/node-semver/compare/v7.6.3...v7.7.1)
    
    ---
    updated-dependencies:
    - dependency-name: semver
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * docs: add partials for AWS permissions
    
    * docs: add partials for azure permissions
    
    * docs: add required-permissions-data repo as a submodule
    
    * Revert "docs: add required-permissions-data repo as a submodule"
    
    This reverts commit 4f4a6e654234a6f61335dacfb584a39695b4df80.
    
    * chore: add permissions-sync script and ghA workflow
    
    * chore: add cd command
    
    * chore: debug ci
    
    * chore: debug ci
    
    * chore: debug ci
    
    * chore: debug ci
    
    * chore: debug ci
    
    * chore: debug ci
    
    * chore: add backport label logic
    
    * chore: fix grep label logic and add PR logic
    
    * chore: ad github token to pr step
    
    * chore: fix branch name
    
    * chore: update backport label logic
    
    * chore: create string of labels and add slack notifications
    
    * chore: add extra condition
    
    * chore: fix condition
    
    * chore: complete map file and improve comments
    
    * chore: remove push trigger
    
    * docs: test suggestions
    
    * docs: add vault token back
    
    * docs: apply suggestions from review
    
    * docs: move last update date to partials and do one last test
    
    * docs: final commit
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    (cherry picked from commit 94850db46d2547fe8bd2aae16072f672cf37ab9b)

---
+++ scripts/permissions-sync.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+# This script compares IAM permission files from two different repositories:
+# - Librarium (target repository)
+# - required-permissions-data (source of truth repository)
+# If any differences are detected between the files, it updates the corresponding MDX file in the Librarium repository.
+
+# Exit if a command fails
+set -e
+
+echo "Starting permissions sync..."
+
+# Define the paths to the repositories and ensure the required variables are set
+repo_librarium="${GITHUB_WORKSPACE}"
+repo_permissions="${PATH_PERMISSIONS:?Env variable PATH_PERMISSIONS in permissions-update.yaml is not set}"
+
+# Check if both repository paths are valid directories
+if [[ ! -d "$repo_librarium" ]] || [[ ! -d "$repo_permissions" ]]; then
+    echo "Error: One or both repositories are missing. Check the paths and try again."
+    exit 1
+fi
+
+# Define the path to the JSON mapping file and check if it exists
+permissions_map=$repo_librarium/scripts/permissions-map.json
+if [[ ! -f "$permissions_map" ]]; then
+    echo "Error: Mapping file '$permissions_map' not found."
+    exit 1
+fi
+
+# Track if any changes are detected with a temp file, as variables don't persist in subshells
+echo "false" > found_changes.txt
+
+# Process each entry in the JSON map: each entry consists of a JSON file and the corresponding MDX file
+jq -r 'to_entries[] | "\(.key) \(.value)"' "$permissions_map" | while read -r json_file mdx_file; do
+    
+    # Check if both the source JSON file and the target MDX file exist
+    if [[ ! -f "$repo_permissions/$json_file" || ! -f "$repo_librarium/$mdx_file" ]]; then
+        echo "Warning: '$json_file' or '$mdx_file' missing. Skipping..."
+        continue
+    fi
+    
+    # Save the MDX file's partials header
+    partials_header=$(awk '{print} /^```(json|yaml|yml)/ {exit}' $repo_librarium/$mdx_file)
+    
+    # Remove the header and footer from the MDX file to isolate the JSON content and save it to a file
+    awk 'f{if ($0 ~ /^```$/) exit; print} /^\s*```(json|yaml|yml)/ {f=1}' $repo_librarium/$mdx_file > mdx_extracted.json
+
+    # Compare the extracted MDX JSON content with the source JSON file
+    if ! diff -q "$repo_permissions/$json_file" mdx_extracted.json; then       
+        echo "true" > found_changes.txt
+        echo "Changes detected in $json_file. Updating $mdx_file..."
+
+        # Update the MDX file with the JSON content
+        echo "$partials_header" > $repo_librarium/$mdx_file
+        cat $repo_permissions/$json_file >> $repo_librarium/$mdx_file
+        echo '```' >> $repo_librarium/$mdx_file
+    fi
+
+    # Clean up the JSON temp file
+    rm mdx_extracted.json
+done
+
+# Check if any changes were detected
+found_changes=$(cat found_changes.txt)
+rm found_changes.txt
+
+# If changes were detected, export the result as a variable for GitHub Actions
+if [ "$found_changes" = true ]; then
+    echo "CHANGES_DETECTED=true" >> $GITHUB_ENV
+    echo "✅ Permission files updated."
+else
+    echo "✅ No changes detected."
+fi
