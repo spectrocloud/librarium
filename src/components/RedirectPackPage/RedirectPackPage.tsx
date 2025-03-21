7311204fe3acfbc47788572a556b5574b0a8d079
addetz <43963729+addetz@users.noreply.github.com>
2025-03-21 19:57:46 +0000

    docs: rename component

--- src/components/RedirectPackPage/RedirectPackPage.tsx
+++ src/components/RedirectPackPage/RedirectPackPage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Redirect } from "@docusaurus/router";
 
-export default function IntegrationsRedirect({ packName }: { packName: string }) {
+export default function RedirectPackPage({ packName }: { packName: string }) {
   return <Redirect to={`/integrations/packs/?pack=${packName}`} />;
 }
