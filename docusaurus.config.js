a24a835a89477a83327fe868031b1d646c1a4404
Amanda Churi Filanowski <amanda.filanowski@spectrocloud.com>
2025-07-28 07:43:06 -0400

    [version-4-4] chore: backport/version-4-4/pr-7502

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -119,13 +119,18 @@ const config = {
     locales: ["en"],
   },
   future: {
+    v4: {
+      removeLegacyPostBuildHeadAttribute: true,
+      useCssCascadeLayers: true, // Slightly decreases padding of admonitions when set to `true`.
+    },
     experimental_faster: {
-      swcJsLoader: false,
-      // Set to 'false' as Netlify builds fail with this enabled.
+      ssgWorkerThreads: false, // Set to 'false' as Netlify builds fail with this enabled.
+      swcJsLoader: false, // Set to 'false' as Netlify builds fail with this enabled.
       swcJsMinimizer: true,
       swcHtmlMinimizer: true,
       lightningCssMinimizer: true,
       rspackBundler: true,
+      rspackPersistentCache: true,
       mdxCrossCompilerCache: true,
     },
   },
