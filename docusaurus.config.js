e39f289a465d034c980bfb609fd4439065b0a2e6
Ben Radstone <benjamin.radstone@spectrocloud.com>
2025-10-22 11:11:34 +0100

    chore: trial ssgWorkerThreads & swcJsLoader enablement

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -136,8 +136,8 @@ const config = {
       useCssCascadeLayers: true, // Slightly decreases padding of admonitions when set to `true`.
     },
     experimental_faster: {
-      ssgWorkerThreads: false, // Set to 'false' as Netlify builds fail with this enabled.
-      swcJsLoader: false, // Set to 'false' as Netlify builds fail with this enabled.
+      ssgWorkerThreads: true, // Set to 'false' as Netlify builds fail with this enabled.
+      swcJsLoader: true, // Set to 'false' as Netlify builds fail with this enabled.
       swcJsMinimizer: true,
       swcHtmlMinimizer: true,
       lightningCssMinimizer: true,
