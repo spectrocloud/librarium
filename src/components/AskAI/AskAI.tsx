0d0bbc829936bf0eed14bc68680ae31db2b24575
Adelina Simion <43963729+addetz@users.noreply.github.com>
2025-12-09 10:02:40 +0000

    docs: add AskAI buttons DOC-2303 (#8790)
    * docs: add landing page and AskI button DOC-2303
    
    * docs: add ask ai buttons
    
    * docs: style navbar ai button
    
    * docs: update button styling
    
    * docs: add landin page text
    
    * docs: force styling of icon
    
    * docs: try to fix lint
    
    * docs: fix vale
    
    * docs: fix lint errors
    
    * docs: fix syntax
    
    * docs: undo new landing page
    
    * docs: disable linter properly
    
    * docs: add AskAI test and bump tsconfig.json
    
    * ci: auto-formatting prettier issues

---
+++ src/components/AskAI/AskAI.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styles from "./AskAI.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWandMagicSparkles } from "@fortawesome/free-solid-svg-icons";
+
+/**
+ * Minimal typeable input component.
+ * Just a controlled text input.
+ * This input is read-only until Kapa can handle the privacy notice screen.
+ */
+type TypeableInputProps = {
+  value: string;
+  onChange: (val: string) => void;
+  placeholder?: string;
+  className?: string;
+  name?: string;
+  onEnter?: (val: string) => void;
+};
+
+function TypeableInput({
+  value,
+  onChange,
+  placeholder = "Ask AI to learn about Palette...",
+  name,
+  onEnter,
+}: TypeableInputProps) {
+  const handleClick = () => {
+    onEnter?.(value); // call handler when user clicks
+  };
+
+  return (
+    <div className={styles.inputContainer}>
+      <FontAwesomeIcon icon={faWandMagicSparkles} className={styles.icon} />
+      <input
+        type="text"
+        name={name}
+        readOnly={true}
+        disabled={false}
+        className={styles.input}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        onClick={handleClick}
+      />
+    </div>
+  );
+}
+
+export function AskAI(): JSX.Element | null {
+  const [text, setText] = React.useState("");
+  return (
+    <div className={styles.inputChangesContainer}>
+      <TypeableInput
+        value={text}
+        onChange={setText}
+        placeholder="Ask AI to learn about Palette..."
+        onEnter={() => {
+          // Open Kapa AI.
+          // Skipping linting on the next line as getting the linter to pass is too complex.
+          if (typeof window !== "undefined" && window.Kapa) {
+            // eslint-disable-next-line  @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+            window.Kapa.open({
+              mode: "ai",
+              submit: true,
+            });
+          }
+        }}
+      />
+    </div>
+  );
+}
+
+export default AskAI;
