60623aae3ab190aae8e76ede995f057c490febd5
Adelina Simion <43963729+addetz@users.noreply.github.com>
2025-12-09 10:38:27 +0000

    docs: add AskAI buttons DOC-2303 (#8790) (#8944)
    * docs: add landing page and AskI button DOC-2303
    
    * docs: add ask ai buttons
    
    * docs: style navbar ai button
    
    * docs: update button styling
    
    * docs: add landin page text
    
    * docs: force styling of icon
    
    * docs: try to fix lint
    
    * docs: fix vale
    
    * docs: fix lint errors
    
    * docs: fix syntax
    
    * docs: undo new landing page
    
    * docs: disable linter properly
    
    * docs: add AskAI test and bump tsconfig.json
    
    * ci: auto-formatting prettier issues

--- src/theme/MDXComponents/MDXComponents.ts
+++ src/theme/MDXComponents/MDXComponents.ts
@@ -20,6 +20,7 @@ import Accordion from "@site/src/components/Accordion";
 import AccordionPanel from "@site/src/components/AccordionPanel";
 import RedirectPackPage from "@site/src/components/RedirectPackPage";
 import OsCveTable from "@site/src/components/OsCveTable/OsCveTable";
+import AskAI from "@site/src/components/AskAI/index";
 
 export default {
   ...MDXComponents,
@@ -44,4 +45,5 @@ export default {
   PaletteVertexUrlMapper,
   RedirectPackPage,
   OsCveTable,
+  AskAI,
 };
