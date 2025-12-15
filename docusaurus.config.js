f1d077a4a6597da548a07202314528b244ce94e8
Adelina Simion <43963729+addetz@users.noreply.github.com>
2025-12-15 16:08:47 +0000

    docs: fix up broken postman links DOC-2421 (#8968)
    * docs: move API files to static folders DOC-2421
    
    * docs: fix up broken postman links DOC-2421
    
    * docs: update wording
    
    * docs: fix up download
    
    * Update docs/api-content/api-docs/3-postman-collection.md
    
    Co-authored-by: Linus Bourque <linus@spectrocloud.com>
    
    * ci: auto-formatting prettier issues
    
    ---------
    
    Co-authored-by: Linus Bourque <linus@spectrocloud.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -246,10 +246,9 @@ const config = {
         docsPluginId: "api",
         config: {
           palette: {
-            specPath: "docs/api-content/api-docs/v1/api.json",
+            specPath: "static/apis/v1/api.json",
             outputDir: "docs/api-content/api-docs/v1",
-            downloadUrl:
-              "https://github.com/spectrocloud/librarium/blob/master/docs/api-content/api-docs/palette-apis.json",
+            downloadUrl: "/apis/v1/palette-apis.json",
             sidebarOptions: {
               groupPathsBy: "tag",
               categoryLinkSource: "tag",
@@ -259,10 +258,9 @@ const config = {
             hideSendButton: true,
           },
           emc: {
-            specPath: "docs/api-content/api-docs/edge-v1/emc-api.json",
+            specPath: "static/apis/edge-v1/emc-api.json",
             outputDir: "docs/api-content/api-docs/edge-v1",
-            downloadUrl:
-              "https://github.com/spectrocloud/librarium/blob/master/docs/api-content/api-docs/palette-apis.json",
+            downloadUrl: "/apis/edge-v1/emc-api.json",
             sidebarOptions: {
               groupPathsBy: "tag",
               categoryLinkSource: "tag",
