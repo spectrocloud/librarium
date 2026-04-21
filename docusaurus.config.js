a3ccde330e68dc69957740c6c4c1d9fe02c65bb8
Adelina Simion <43963729+addetz@users.noreply.github.com>
2026-04-21 13:44:02 +0100

    docs: add the Docs navbar entry again DOC-2764 (#10148)
    (cherry picked from commit 7664d277ef0232d7b6e3e717e300bae3a327eb39)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -364,6 +364,14 @@ const config = {
         },
         items: [
           {
+            to: "/",
+            type: "docSidebar",
+            sidebarId: "docSidebar",
+            label: "Docs",
+            position: "left",
+            activeBaseRegex: "^(?!/api/).*$",
+          },
+          {
             to: "/tutorials",
             type: "docSidebar",
             sidebarId: "tutorialSidebar",
